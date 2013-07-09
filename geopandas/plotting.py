8603688798880241cb61953df5030555866e8c3e
Kelsey Jordahl <kjordahl@alum.mit.edu>
2013-07-09 19:51:26 +0530

    Refactor plotting into a separate module

---
+++ geopandas/plotting.py
@@ -0,0 +1,60 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from descartes.patch import PolygonPatch
+
+def plot_polygon(ax, poly, facecolor='red', edgecolor='black', alpha=0.5):
+    a = np.asarray(poly.exterior)
+    # without Descartes, we could make a Patch of exterior
+    ax.add_patch(PolygonPatch(poly, facecolor=facecolor, alpha=alpha))
+    ax.plot(a[:, 0], a[:, 1], color=edgecolor)
+    for p in poly.interiors:
+        x, y = zip(*p.coords)
+        ax.plot(x, y, color=edgecolor)
+
+
+def plot_multipolygon(ax, geom, facecolor='red'):
+    """ Can safely call with either Polygon or Multipolygon geometry
+    """
+    if geom.type == 'Polygon':
+        plot_polygon(ax, geom, facecolor)
+    elif geom.type == 'MultiPolygon':
+        for poly in geom.geoms:
+            plot_polygon(ax, poly, facecolor=facecolor)
+
+
+def plot_point(ex, geom):
+    """ TODO
+    """
+    pass
+
+
+def gencolor(N, colormap='Set1'):
+    """
+    Color generator intended to work with one of the ColorBrewer
+    qualitative color scales.
+
+    Suggested values of colormap are the following:
+
+        Accent, Dark2, Paired, Pastel1, Pastel2, Set1, Set2, Set3
+
+    (although any matplotlib colormap will work).
+    """
+    # don't use more than 9 discrete colors
+    n_colors = min(N, 9)
+    cmap = cm.get_cmap(colormap, n_colors)
+    colors = cmap(range(n_colors))
+    for i in xrange(N):
+        yield colors[i % n_colors]
+
+def plot_series(s, colormap='Set1'):
+    fig = plt.figure()
+    fig.add_subplot(111, aspect='equal')
+    ax = plt.gca()
+    color = gencolor(len(s), colormap=colormap)
+    for geom in s:
+        if geom.type == 'Polygon' or geom.type == 'MultiPolygon':
+            plot_multipolygon(ax, geom, facecolor=color.next())
+        elif geom.type == 'Point':
+            plot_point(ax, geom)
+    return ax
