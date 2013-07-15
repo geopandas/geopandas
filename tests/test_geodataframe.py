e1f0d9a5afe9b0fbc831ef7181f75aa57547c06b
Kelsey Jordahl <kjordahl@alum.mit.edu>
2013-07-15 11:28:59 +0530

    GeoDataFrame test to unittest framework

--- tests/test_geodataframe.py
+++ tests/test_geodataframe.py
@@ -1,19 +1,22 @@
+import unittest
 import json
 
 from geopandas import GeoDataFrame
 
-def test_from_file_():
-    # Data from http://www.nyc.gov/html/dcp/download/bytes/nybb_13a.zip
-    # saved as geopandas/examples/nybb_13a.zip.
-    df = GeoDataFrame.from_file(
-        '/nybb_13a/nybb.shp', vfs='zip://examples/nybb_13a.zip')
-    assert 'geometry' in df
-    assert len(df) == 5
+class TestSeries(unittest.TestCase):
 
-def test_to_json():
-    df = GeoDataFrame.from_file(
-        '/nybb_13a/nybb.shp', vfs='zip://examples/nybb_13a.zip')
-    text = df.to_json()
-    data = json.loads(text)
-    assert data['type'] == 'FeatureCollection'
-    assert len(data['features']) == 5
+    def setUp(self):
+        # Data from http://www.nyc.gov/html/dcp/download/bytes/nybb_13a.zip
+        # saved as geopandas/examples/nybb_13a.zip.
+        self.df = GeoDataFrame.from_file(
+            '/nybb_13a/nybb.shp', vfs='zip://examples/nybb_13a.zip')
+
+    def test_from_file_(self):
+        assert 'geometry' in self.df
+        assert len(self.df) == 5
+    
+    def test_to_json(self):
+        text = self.df.to_json()
+        data = json.loads(text)
+        assert data['type'] == 'FeatureCollection'
+        assert len(data['features']) == 5
