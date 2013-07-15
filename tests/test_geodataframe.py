36332c5caacd187c6b05ac9c973ef2bf27c83e0a
Kelsey Jordahl <kjordahl@alum.mit.edu>
2013-07-15 11:40:46 +0530

    Actually use unittest assert methods

--- tests/test_geodataframe.py
+++ tests/test_geodataframe.py
@@ -12,11 +12,11 @@ class TestSeries(unittest.TestCase):
             '/nybb_13a/nybb.shp', vfs='zip://examples/nybb_13a.zip')
 
     def test_from_file_(self):
-        assert 'geometry' in self.df
-        assert len(self.df) == 5
+        self.assertTrue('geometry' in self.df)
+        self.assertTrue(len(self.df) == 5)
     
     def test_to_json(self):
         text = self.df.to_json()
         data = json.loads(text)
-        assert data['type'] == 'FeatureCollection'
-        assert len(data['features']) == 5
+        self.assertTrue(data['type'] == 'FeatureCollection')
+        self.assertTrue(len(data['features']) == 5)
--- tests/test_geoseries.py
+++ tests/test_geoseries.py
@@ -45,40 +45,40 @@ class TestSeries(unittest.TestCase):
         assert b[1].equals(l2)
 
     def test_bounds(self):
-        assert np.allclose(self.g1.bounds.values, np.array([[0, 0, 1, 1],
-                                                            [0, 0, 1, 1]]))
+        self.assertTrue(np.allclose(self.g1.bounds.values, np.array([[0, 0, 1, 1],
+                                                            [0, 0, 1, 1]])))
 
     def test_contains(self):
-        assert np.alltrue(self.g1.contains(self.t1))
-        assert not np.alltrue(self.g1.contains(Point([5, 5])))
+        self.assertTrue(np.alltrue(self.g1.contains(self.t1)))
+        self.assertFalse(np.alltrue(self.g1.contains(Point([5, 5]))))
 
     def test_length(self):
         l = np.array([2 + np.sqrt(2), 4])
-        assert np.allclose(self.g1.length.values, l)
+        self.assertTrue(np.allclose(self.g1.length.values, l))
 
     def test_equals(self):
-        assert np.alltrue(self.g1.equals(self.g1))
-        assert np.all(self.g1.equals(self.sq).values == np.array([0, 1], dtype=bool))
+        self.assertTrue(np.alltrue(self.g1.equals(self.g1)))
+        self.assertTrue(np.all(self.g1.equals(self.sq).values == np.array([0, 1], dtype=bool)))
 
     def test_equals_align(self):
         a = self.a1.equals(self.a2)
-        assert a['A'] == False
-        assert a['B'] == True
-        assert a['C'] == False
+        self.assertFalse(a['A'])
+        self.assertTrue(a['B'])
+        self.assertFalse(a['C'])
 
     def test_align(self):
         a1, a2 = self.a1.align(self.a2)
-        assert a2['A'].is_empty
-        assert a1['B'].equals(a2['B'])
-        assert a1['C'].is_empty
+        self.assertTrue(a2['A'].is_empty)
+        self.assertTrue(a1['B'].equals(a2['B']))
+        self.assertTrue(a1['C'].is_empty)
 
     def test_almost_equals(self):
-        assert np.alltrue(self.g1.equals(self.g1))
-        assert np.all(self.g1.equals(self.sq).values == np.array([0, 1], dtype=bool))
+        self.assertTrue(np.alltrue(self.g1.equals(self.g1)))
+        self.assertTrue(np.all(self.g1.equals(self.sq).values == np.array([0, 1], dtype=bool)))
 
     def test_equals_exact(self):
-        assert np.alltrue(self.g1.equals(self.g1))
-        assert np.all(self.g1.equals(self.sq).values == np.array([0, 1], dtype=bool))
+        self.assertTrue(np.alltrue(self.g1.equals(self.g1)))
+        self.assertTrue(np.all(self.g1.equals(self.sq).values == np.array([0, 1], dtype=bool)))
 
     def test_crosses(self):
         # TODO
@@ -105,56 +105,56 @@ class TestSeries(unittest.TestCase):
         pass
 
     def test_intersection(self):
-        assert geom_equals(self.g1 & self.g2, self.t1)
+        self.assertTrue(geom_equals(self.g1 & self.g2, self.t1))
 
     def test_union_series(self):
         u = self.g1.union(self.g2)
-        assert u[0].equals(self.sq)
-        assert u[1].equals(self.sq)
-        assert geom_equals(u, self.g1 | self.g2)
+        self.assertTrue(u[0].equals(self.sq))
+        self.assertTrue(u[1].equals(self.sq))
+        self.assertTrue(geom_equals(u, self.g1 | self.g2))
 
     def test_union_polgon(self):
         u = self.g1.union(self.t2)
-        assert u[0].equals(self.sq)
-        assert u[1].equals(self.sq)
+        self.assertTrue(u[0].equals(self.sq))
+        self.assertTrue(u[1].equals(self.sq))
 
     def test_symmetric_difference_series(self):
         u = self.g3.symmetric_difference(self.g4)
-        assert u[0].equals(self.sq)
-        assert u[1].equals(self.sq)
-        assert geom_equals(u, self.g3 ^ self.g4)
+        self.assertTrue(u[0].equals(self.sq))
+        self.assertTrue(u[1].equals(self.sq))
+        self.assertTrue(geom_equals(u, self.g3 ^ self.g4))
 
     def test_symmetric_difference_poly(self):
         u = self.g3.symmetric_difference(self.t1)
-        assert u[0].is_empty
-        assert u[1].equals(self.sq)
+        self.assertTrue(u[0].is_empty)
+        self.assertTrue(u[1].equals(self.sq))
 
     def test_difference_series(self):
         u = self.g1.difference(self.g2)
-        assert u[0].is_empty
-        assert u[1].equals(self.t2)
-        assert geom_equals(u, self.g1 - self.g2)
+        self.assertTrue(u[0].is_empty)
+        self.assertTrue(u[1].equals(self.t2))
+        self.assertTrue(geom_equals(u, self.g1 - self.g2))
 
     def test_difference_poly(self):
         u = self.g1.difference(self.t2)
-        assert u[0].equals(self.t1)
-        assert u[1].equals(self.t1)
+        self.assertTrue(u[0].equals(self.t1))
+        self.assertTrue(u[1].equals(self.t1))
 
     def test_is_valid(self):
-        assert np.alltrue(self.g1.is_valid)
+        self.assertTrue(np.alltrue(self.g1.is_valid))
 
     def test_is_empty(self):
-        assert np.alltrue(np.logical_not(self.g1.is_empty))
+        self.assertTrue(np.alltrue(np.logical_not(self.g1.is_empty)))
 
     def test_is_ring(self):
-        assert np.alltrue(self.g1.is_ring)
+        self.assertTrue(np.alltrue(self.g1.is_ring))
 
     def test_is_simple(self):
-        assert np.alltrue(self.g1.is_simple)
+        self.assertTrue(np.alltrue(self.g1.is_simple))
 
     def test_envelope(self):
         e = self.g3.envelope
-        assert np.alltrue(e.equals(self.sq))
+        self.assertTrue(np.alltrue(e.equals(self.sq)))
 
     def test_exterior(self):
         # TODO
@@ -165,7 +165,7 @@ class TestSeries(unittest.TestCase):
         pass
 
     def test_representative_point(self):
-        assert np.alltrue(self.g1.contains(self.g1.representative_point()))
-        assert np.alltrue(self.g2.contains(self.g2.representative_point()))
-        assert np.alltrue(self.g3.contains(self.g3.representative_point()))
-        assert np.alltrue(self.g4.contains(self.g4.representative_point()))
+        self.assertTrue(np.alltrue(self.g1.contains(self.g1.representative_point())))
+        self.assertTrue(np.alltrue(self.g2.contains(self.g2.representative_point())))
+        self.assertTrue(np.alltrue(self.g3.contains(self.g3.representative_point())))
+        self.assertTrue(np.alltrue(self.g4.contains(self.g4.representative_point())))
