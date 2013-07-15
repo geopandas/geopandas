4733055d8eee5e0e3ca3bd47eaa5c776bb62c9a8
Kelsey Jordahl <kjordahl@alum.mit.edu>
2013-07-15 11:44:02 +0530

    Add test for values of text columns in GeoDataFrame from file

--- tests/test_geodataframe.py
+++ tests/test_geodataframe.py
@@ -1,9 +1,10 @@
 import unittest
 import json
+import numpy as np
 
 from geopandas import GeoDataFrame
 
-class TestSeries(unittest.TestCase):
+class TestDataFrame(unittest.TestCase):
 
     def setUp(self):
         # Data from http://www.nyc.gov/html/dcp/download/bytes/nybb_13a.zip
@@ -14,6 +15,8 @@ class TestSeries(unittest.TestCase):
     def test_from_file_(self):
         self.assertTrue('geometry' in self.df)
         self.assertTrue(len(self.df) == 5)
+        self.assertTrue(np.alltrue(self.df['BoroName'].values == np.array(['Staten Island',
+                                   'Queens', 'Brooklyn', 'Manhattan', 'Bronx'])))
     
     def test_to_json(self):
         text = self.df.to_json()
