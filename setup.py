e5980e34d1e079be060ccaba5c13fd8b8b70a7b7
Kelsey Jordahl <kjordahl@alum.mit.edu>
2013-07-31 22:27:30 -0400

    You got got markdown in my rst.

--- setup.py
+++ setup.py
@@ -4,14 +4,17 @@ except ImportError:
     from distutils.core import setup
 
 LONG_DESCRIPTION = """GeoPandas is a project to add support for geographic data to
-[pandas](http://pandas.pydata.org) objects.
+`pandas`_ objects.
 
 The goal of GeoPandas is to make working with geospatial data in
 python easier. It combines the capabilities of pandas and shapely,
 providing geospatial operations in pandas and a high-level interface
 to multiple geometries to shapely. GeoPandas enables you to easily do
 operations in python that would otherwise require a spatial database
-such as PostGIS."""
+such as PostGIS.
+
+.. _pandas: http://pandas.pydata.org
+"""
 
 setup(name='geopandas',
       version='0.1dev',
