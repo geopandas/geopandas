4ab1f64673b3287c7594471673f87a5c7d73411f
Kelsey Jordahl <kjordahl@alum.mit.edu>
2013-07-31 21:59:45 -0400

    Update setup.py for upload to PyPI

--- setup.py
+++ setup.py
@@ -3,13 +3,24 @@ try:
 except ImportError:
     from distutils.core import setup
 
+LONG_DESCRIPTION = """GeoPandas is a project to add support for geographic data to
+[pandas](http://pandas.pydata.org) objects.
+
+The goal of GeoPandas is to make working with geospatial data in
+python easier. It combines the capabilities of pandas and shapely,
+providing geospatial operations in pandas and a high-level interface
+to multiple geometries to shapely. GeoPandas enables you to easily do
+operations in python that would otherwise require a spatial database
+such as PostGIS."""
+
 setup(name='geopandas',
       version='0.1dev',
       description='Geographic pandas extensions',
       license='BSD',
       author='Kelsey Jordahl',
       author_email='kjordahl@enthought.com',
-      url='',
+      url='http://geopandas.org',
+      long_description=LONG_DESCRIPTION,
       packages=['geopandas'],
       install_requires=['pandas', 'shapely', 'fiona', 'descartes'],
 )
