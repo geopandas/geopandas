3875baec2107a4b289defac33cf8d55bebb06f5b
Kelsey Jordahl <kjordahl@alum.mit.edu>
2013-08-07 18:50:11 -0400

    Use temp directory for test_to_file

--- tests/test_geodataframe.py
+++ tests/test_geodataframe.py
@@ -1,10 +1,13 @@
 import unittest
 import json
+import os
+import tempfile
+import shutil
+
 import numpy as np
 
-import tempfile
-
 from geopandas import GeoDataFrame
+
 
 class TestDataFrame(unittest.TestCase):
 
@@ -13,6 +16,10 @@ class TestDataFrame(unittest.TestCase):
         # saved as geopandas/examples/nybb_13a.zip.
         self.df = GeoDataFrame.from_file(
             '/nybb_13a/nybb.shp', vfs='zip://examples/nybb_13a.zip')
+        self.tempdir = tempfile.mkdtemp()
+
+    def tearDown(self):
+        shutil.rmtree(self.tempdir)
 
     def test_from_file_(self):
         self.assertTrue('geometry' in self.df)
@@ -27,11 +34,11 @@ class TestDataFrame(unittest.TestCase):
         self.assertTrue(len(data['features']) == 5)
         
     def test_to_file(self):
-        with tempfile.NamedTemporaryFile(suffix='.shp') as t:
-            self.df.to_file(t.name)
-            # Read layer back in?
-            df = GeoDataFrame.from_file(t.name)
-            self.assertTrue('geometry' in df)
-            self.assertTrue(len(df) == 5)
-            self.assertTrue(np.alltrue(df['BoroName'].values == np.array(['Staten Island',
+        tempfilename = os.path.join(self.tempdir, 'boros.shp')
+        self.df.to_file(tempfilename)
+        # Read layer back in?
+        df = GeoDataFrame.from_file(tempfilename)
+        self.assertTrue('geometry' in df)
+        self.assertTrue(len(df) == 5)
+        self.assertTrue(np.alltrue(df['BoroName'].values == np.array(['Staten Island',
                                    'Queens', 'Brooklyn', 'Manhattan', 'Bronx'])))
