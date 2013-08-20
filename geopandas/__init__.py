3191269b3da0766cdca21721b49e6dda0390c54c
Kelsey Jordahl <kjordahl@alum.mit.edu>
2013-08-19 23:29:10 -0400

    BUG: Don't fail for missing version file

--- geopandas/__init__.py
+++ geopandas/__init__.py
@@ -1,3 +1,6 @@
-from geopandas.version import version as __version__
+try:
+    from geopandas.version import version as __version__
+except ImportError:
+    __version__ = '0.1.0.dev-unknown'
 from geoseries import GeoSeries
 from geodataframe import GeoDataFrame
