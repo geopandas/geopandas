afaacc56606e2257ad001521dc717ee745213dbd
Kelsey Jordahl <kjordahl@alum.mit.edu>
2013-10-16 07:53:08 -0400

    DOC: Copyright to GeoPandas developers

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'GeoPandas'
-copyright = u'2013, Kelsey Jordahl'
+copyright = u'2013, GeoPandas developers'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
