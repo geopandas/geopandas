1f6dfbde172a604f0d486f9b62e4697851d8055d
Jacob Wasserman <jwasserman@gmail.com>
2013-11-06 10:09:23 -0500

    Move tests to test_geom_methods.py
    For geometry tests, test both the GeoSeries and GeoDataFrame versions.
    
    If the method's input is also a GeoSeries, test the input for both
    GeoSeries and GeoDataFrame as well.
    
    For example, for intersects(), test the following combination of inputs:
    GeoSeries, GeoSeries,
    GeoDataFrame, GeoSeries,
    GeoSeries, GeoDataFrame,
    GeoDataFrame, GeoDataFrame
    
    Helper functions are provided to test all combinations

--- tests/test_geoseries.py
+++ tests/test_geoseries.py
@@ -65,10 +65,6 @@ class TestSeries(unittest.TestCase):
             for x in gs:
                 self.assert_(x is g)
 
-    def test_area(self):
-        self.assertTrue(type(self.g1.area) is Series)
-        assert_array_equal(self.g1.area.values, np.array([0.5, 1.0]))
-
     def test_copy(self):
         gc = self.g3.copy()
         self.assertTrue(type(gc) is GeoSeries)
@@ -82,25 +78,6 @@ class TestSeries(unittest.TestCase):
         self.assertTrue(self.t2 in self.g3)
         self.assertTrue(self.sq not in self.g3)
         self.assertTrue(5 not in self.g3)
-
-    def test_boundary(self):
-        l1 = LineString([(0, 0), (1, 0), (1, 1), (0, 0)])
-        l2 = LineString([(0, 0), (1, 0), (1, 1), (0, 1), (0, 0)])
-        b = self.g1.boundary
-        self.assertTrue(b[0].equals(l1))
-        self.assertTrue(b[1].equals(l2))
-
-    def test_bounds(self):
-        assert_array_equal(self.g1.bounds.values, np.array([[0, 0, 1, 1],
-                                                            [0, 0, 1, 1]]))
-
-    def test_contains(self):
-        self.assertTrue(np.alltrue(self.g1.contains(self.t1)))
-        self.assertFalse(np.alltrue(self.g1.contains(Point([5, 5]))))
-
-    def test_length(self):
-        l = np.array([2 + np.sqrt(2), 4])
-        assert_array_equal(self.g1.length.values, l)
 
     def test_equals(self):
         self.assertTrue(np.alltrue(self.g1.equals(self.g1)))
@@ -128,31 +105,6 @@ class TestSeries(unittest.TestCase):
         self.assertTrue(np.alltrue(self.g1.equals_exact(self.g1, 0.001)))
         assert_array_equal(self.g1.equals_exact(self.sq, 0.001), [False, True])
 
-    @unittest.skip('TODO')
-    def test_crosses(self):
-        # TODO
-        pass
-
-    @unittest.skip('TODO')
-    def test_disjoint(self):
-        # TODO
-        pass
-
-    @unittest.skip('TODO')
-    def test_intersects(self):
-        # TODO
-        pass
-
-    @unittest.skip('TODO')
-    def test_overlaps(self):
-        # TODO
-        pass
-
-    @unittest.skip('TODO')
-    def test_touches(self):
-        # TODO
-        pass
-
     def test_to_file(self):
         """ Test to_file and from_file """
         tempfilename = os.path.join(self.tempdir, 'test.shp')
@@ -161,77 +113,6 @@ class TestSeries(unittest.TestCase):
         s = GeoSeries.from_file(tempfilename)
         self.assertTrue(all(self.g3.equals(s)))
         # TODO: compare crs
-
-    @unittest.skip('TODO')
-    def test_within(self):
-        # TODO
-        pass
-
-    def test_intersection(self):
-        self.assertTrue(geom_equals(self.g1 & self.g2, self.t1))
-
-    def test_union_series(self):
-        u = self.g1.union(self.g2)
-        self.assertTrue(u[0].equals(self.sq))
-        self.assertTrue(u[1].equals(self.sq))
-        self.assertTrue(geom_equals(u, self.g1 | self.g2))
-
-    def test_union_polgon(self):
-        u = self.g1.union(self.t2)
-        self.assertTrue(u[0].equals(self.sq))
-        self.assertTrue(u[1].equals(self.sq))
-
-    def test_symmetric_difference_series(self):
-        u = self.g3.symmetric_difference(self.g4)
-        self.assertTrue(u[0].equals(self.sq))
-        self.assertTrue(u[1].equals(self.sq))
-        self.assertTrue(geom_equals(u, self.g3 ^ self.g4))
-        self.assertEqual(self.g3.crs, u.crs)
-
-    def test_symmetric_difference_poly(self):
-        u = self.g3.symmetric_difference(self.t1)
-        self.assertTrue(u[0].is_empty)
-        self.assertTrue(u[1].equals(self.sq))
-        self.assertEqual(self.g3.crs, u.crs)
-
-    def test_difference_series(self):
-        u = self.g1.difference(self.g2)
-        self.assertTrue(u[0].is_empty)
-        self.assertTrue(u[1].equals(self.t2))
-        self.assertTrue(geom_equals(u, self.g1 - self.g2))
-
-    def test_difference_poly(self):
-        u = self.g1.difference(self.t2)
-        self.assertTrue(u[0].equals(self.t1))
-        self.assertTrue(u[1].equals(self.t1))
-
-    def test_is_valid(self):
-        self.assertTrue(np.alltrue(self.g1.is_valid))
-
-    def test_is_empty(self):
-        self.assertTrue(np.alltrue(np.logical_not(self.g1.is_empty)))
-
-    def test_is_ring(self):
-        self.assertTrue(np.alltrue(self.g1.is_ring))
-
-    def test_is_simple(self):
-        self.assertTrue(np.alltrue(self.g1.is_simple))
-
-    def test_envelope(self):
-        e = self.g3.envelope
-        self.assertTrue(np.alltrue(e.equals(self.sq)))
-        self.assertIsInstance(e, GeoSeries)
-        self.assertEqual(self.g3.crs, e.crs)
-
-    @unittest.skip('TODO')
-    def test_exterior(self):
-        # TODO
-        pass
-
-    @unittest.skip('TODO')
-    def test_interiors(self):
-        # TODO
-        pass
 
     def test_representative_point(self):
         self.assertTrue(np.alltrue(self.g1.contains(self.g1.representative_point())))
@@ -255,55 +136,6 @@ class TestSeries(unittest.TestCase):
         self.assertTrue(geom_equals(self.na_none[:2], na[:2]))
         # XXX: method works inconsistently for different pandas versions
         #self.na_none.fillna(method='backfill')
-
-    def test_interpolate(self):
-        res = self.g5.interpolate(0.75, normalized=True)
-        self.assertTrue(geom_equals(res, GeoSeries([Point(0.5, 1.0),
-                                                    Point(0.75, 1.0)])))
-        res = self.g5.interpolate(1.5)
-        self.assertTrue(geom_equals(res, GeoSeries([Point(0.5, 1.0),
-                                                    Point(1.0, 0.5)])))
-        
-    def test_project(self):
-        res = self.g5.project(Point(1.0, 0.5))
-        assert_array_equal(res, [2.0, 1.5])
-        res = self.g5.project(Point(1.0, 0.5), normalized=True)
-        assert_array_equal(res, [1.0, 0.5])
-        
-    def test_translate_tuple(self):
-        trans = self.sol.x - self.esb.x, self.sol.y - self.esb.y
-        self.assertTrue(self.landmarks.translate(*trans)[0].equals(self.sol))
-    
-    def test_rotate(self):
-        angle = 98
-        res = self.g4.rotate(angle, origin=Point(0,0))
-        self.assertTrue(geom_almost_equals(self.g4, res.rotate(-angle, 
-            origin=Point(0,0))))
-
-    def test_scale(self):
-        scale = 2., 1.
-        inv = tuple(1./i for i in scale)
-        res = self.g4.scale(*scale, origin=Point(0,0))
-        self.assertTrue(geom_almost_equals(self.g4, res.scale(*inv, 
-            origin=Point(0,0))))
-        
-    def test_skew(self):
-        skew = 45.
-        res = self.g4.skew(xs=skew, origin=Point(0,0))
-        self.assertTrue(geom_almost_equals(self.g4, res.skew(xs=-skew, 
-            origin=Point(0,0))))
-        res = self.g4.skew(ys=skew, origin=Point(0,0))
-        self.assertTrue(geom_almost_equals(self.g4, res.skew(ys=-skew, 
-            origin=Point(0,0))))
-            
-    def test_total_bounds(self):
-        bbox = self.sol.x, self.sol.y, self.esb.x, self.esb.y
-        self.assertEqual(self.landmarks.total_bounds, bbox)
-        self.assertEqual(self.g1.total_bounds, (0, 0, 1, 1))
-        s = GeoSeries(Point(x, x) for x in xrange(5))
-        self.assertEqual(s.total_bounds, (0.0, 0.0, 4.0, 4.0))
-        s[0] = Point(10, 10)
-        self.assertEqual(s.total_bounds, (1.0, 1.0, 10.0, 10.0))
 
 if __name__ == '__main__':
     unittest.main()
