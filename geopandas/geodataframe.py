d05dc35077456fc517cb95734cc0bc0d1ccf8bc9
Kelsey Jordahl <kjordahl@enthought.com>
2013-11-11 21:13:46 -0500

    BUG: Everybody likes dictionary comprehensions. Except Python 2.6.

--- geopandas/geodataframe.py
+++ geopandas/geodataframe.py
@@ -211,8 +211,8 @@ class GeoDataFrame(DataFrame):
             return {
                 'id': str(i),
                 'type': 'Feature',
-                'properties': {
-                    k: v for k, v in row.iteritems() if k != 'geometry'},
+                'properties':
+                    dict((k, v) for k, v in row.iteritems() if k != 'geometry'),
                 'geometry': mapping(row['geometry']) }
 
         return json.dumps(
@@ -247,8 +247,8 @@ class GeoDataFrame(DataFrame):
             return {
                 'id': str(i),
                 'type': 'Feature',
-                'properties': {
-                    k: v for k, v in row.iteritems() if k != 'geometry'},
+                'properties':
+                    dict((k, v) for k, v in row.iteritems() if k != 'geometry'),
                 'geometry': mapping(row['geometry']) }
         
         properties = OrderedDict([(col, convert_type(_type)) for col, _type 
