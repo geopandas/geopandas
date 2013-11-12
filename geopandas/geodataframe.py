6798c65a99c667c393f2fc4faeb34c1a13b3b62f
Kelsey Jordahl <kjordahl@enthought.com>
2013-11-11 22:12:26 -0500

    BUG: OrderedDict is not in collections module in python 2.6

--- geopandas/geodataframe.py
+++ geopandas/geodataframe.py
@@ -1,4 +1,8 @@
-from collections import OrderedDict
+try:
+    from collections import OrderedDict
+except ImportError:
+    # Python 2.6
+    from ordereddict import OrderedDict
 import json
 import os
 import sys
