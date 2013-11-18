a99b759ba443e0329df6d9cd4ae7df66ef21425f
Carson Farmer <carson.farmer@gmail.com>
2013-11-17 22:54:18 -0500

    fix typo in test name

--- tests/test_geodataframe.py
+++ tests/test_geodataframe.py
@@ -116,7 +116,7 @@ class TestDataFrame(unittest.TestCase):
         with self.assertRaises(KeyError):
 =======
         # non-geometry error
-        def _should_raise_value_error_with_non_geo():
+        def _should_raise_type_error_with_non_geo():
             df = self.df.copy()
             df.geometry = range(df.shape[0])
         self.assertRaises(TypeError, _should_raise_value_error_with_non_geo)
