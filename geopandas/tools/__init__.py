93e66eeaef15b4d8b392a0709540b9e1a8637d14
Jacob Wasserman <jwasserman@gmail.com>
2014-07-24 10:51:17 -0400

    Move geocode.py to tools/geocoding.py
    Move geocoding capability into the tools module.

--- geopandas/tools/__init__.py
+++ geopandas/tools/__init__.py
@@ -1,10 +1,13 @@
 from __future__ import absolute_import
 
+from .geocoding import geocode, reverse_geocode
 from .overlay import overlay
 from .sjoin import sjoin
 
 __all__ = [
     'overlay',
     'sjoin',
+    'geocode',
+    'reverse_geocode'
 ]
 
