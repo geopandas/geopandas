95bf8c126b88785737ac50cfc9b4e33a00ad8b90
Luke <lucasnad27@gmail.com>
2014-08-14 15:45:35 -0700

    Removed NoneType references for PY3 compatibility

--- geopandas/geodataframe.py
+++ geopandas/geodataframe.py
@@ -3,7 +3,6 @@ try:
 except ImportError:
     # Python 2.6
     from ordereddict import OrderedDict
-from types import NoneType
 import json
 import os
 import sys
@@ -138,7 +137,7 @@ class GeoDataFrame(GeoPandasBase, DataFrame):
             level.crs = crs
 
         # Check that we are using a listlike of geometries
-        if not all(isinstance(item, (BaseGeometry, NoneType)) for item in level):
+        if not all(isinstance(item, BaseGeometry) or not item for item in level):
             raise TypeError("Input geometry column must contain valid geometry objects.")
         frame[geo_column_name] = level
         frame._geometry_column_name = geo_column_name
