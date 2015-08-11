40a72807befcd74b141ca46aeb7994510b546d37
Liam Horne <lihorne@uwaterloo.ca>
2015-08-11 17:00:47 -0400

    import geocoding

--- geopandas/__init__.py
+++ geopandas/__init__.py
@@ -5,6 +5,7 @@ except ImportError:
 
 from geopandas.geoseries import GeoSeries
 from geopandas.geodataframe import GeoDataFrame
+from geopandas.tools.geocoding import geocoding
 
 from geopandas.io.file import read_file
 from geopandas.io.sql import read_postgis
