7fc2b2bd18a615d617d0ef294a6b2b761d35b0e2
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2016-06-09 10:08:48 +0200

    TST: more thorough testing of overlay

--- geopandas/tests/test_overlay.py
+++ geopandas/tests/test_overlay.py
@@ -1,14 +1,20 @@
 from __future__ import absolute_import
 
-from shapely.geometry import Point
+import tempfile
+import shutil
 
-import geopandas
-from geopandas import GeoDataFrame, read_file, overlay
+import numpy as np
 
-import pytest
+from pandas.util.testing import assert_frame_equal
+
+from shapely.geometry import Point, Polygon
+
+from geopandas import GeoDataFrame, GeoSeries, read_file
+from geopandas.tests.util import unittest, download_nybb, assert_geoseries_equal
+from geopandas import overlay
 
 
-class TestDataFrame:
+class TestOverlayNYBB(unittest.TestCase):
 
     def setup_method(self):
         N = 10
@@ -115,3 +121,91 @@ class TestDataFrame:
         # Issue #305
         with pytest.raises(NotImplementedError):
             overlay(self.polydf, self.polydf2.geometry, how="union")
+
+
+class TestOverlay(unittest.TestCase):
+
+    use_sindex = True
+
+    def setUp(self):
+
+        s1 = GeoSeries([Polygon([(0,0), (2,0), (2,2), (0,2)]),
+                        Polygon([(2,2), (4,2), (4,4), (2,4)])])
+        s2 = GeoSeries([Polygon([(1,1), (3,1), (3,3), (1,3)]),
+                        Polygon([(3,3), (5,3), (5,5), (3,5)])])
+
+        self.df1 = GeoDataFrame({'geometry': s1, 'col1':[1,2]})
+        self.df2 = GeoDataFrame({'geometry': s2, 'col2':[1,2]})
+
+        self.result  = GeoDataFrame(
+            {'col1': [1, 1, np.nan, np.nan, 2, 2, 2, np.nan, 2],
+             'col2': [np.nan, 1, 1, 1, np.nan, 1, np.nan, 2, 2],
+             'geometry': [Polygon([(2, 1), (2, 0), (0, 0), (0, 2), (1, 2), (1, 1), (2, 1)]),
+                          Polygon([(2, 1), (1, 1), (1, 2), (2, 2), (2, 1)]),
+                          Polygon([(2, 1), (2, 2), (3, 2), (3, 1), (2, 1)]),
+                          Polygon([(2, 2), (1, 2), (1, 3), (2, 3), (2, 2)]),
+                          Polygon([(3, 2), (3, 3), (4, 3), (4, 2), (3, 2)]),
+                          Polygon([(3, 3), (3, 2), (2, 2), (2, 3), (3, 3)]),
+                          Polygon([(3, 3), (2, 3), (2, 4), (3, 4), (3, 3)]),
+                          Polygon([(4, 3), (4, 4), (3, 4), (3, 5), (5, 5), (5, 3), (4, 3)]),
+                          Polygon([(3, 4), (4, 4), (4, 3), (3, 3), (3, 4)])]
+            })
+
+    def test_union(self):
+        res = overlay(self.df1, self.df2, how='union',
+                      use_sindex=self.use_sindex)
+        exp = self.result
+        assert_frame_equal(res, exp)
+        assert_geoseries_equal(res.geometry, exp.geometry)
+
+    def test_intersection(self):
+        res = overlay(self.df1, self.df2, how='intersection',
+                      use_sindex=self.use_sindex)
+        exp = self.result.dropna(subset=['col1', 'col2'], how='any')
+        exp = exp.reset_index(drop=True)
+        exp[['col1', 'col2']] = exp[['col1', 'col2']].astype('int64')
+        assert_frame_equal(res, exp)
+        assert_geoseries_equal(res.geometry, exp.geometry)
+
+    def test_symdiff(self):
+        res = overlay(self.df1, self.df2, how='symmetric_difference',
+                      use_sindex=self.use_sindex)
+        exp = self.result[self.result[['col1', 'col2']].isnull().sum(1) == 1]
+        exp = exp.reset_index(drop=True)
+        assert_frame_equal(res, exp)
+        assert_geoseries_equal(res.geometry, exp.geometry)
+
+    def test_difference(self):
+        res = overlay(self.df1, self.df2, how='difference',
+                      use_sindex=self.use_sindex)
+        exp = self.result.loc[[0, 4, 6]]
+        exp = exp.reset_index(drop=True)
+        exp['col1'] = exp['col1'].astype('int64')
+        exp['col2'] = np.array([None, None, None], dtype='O')
+        assert_frame_equal(res, exp)
+        assert_geoseries_equal(res.geometry, exp.geometry)
+
+    def test_identity(self):
+        res = overlay(self.df1, self.df2, how='identity',
+                      use_sindex=self.use_sindex)
+        exp = self.result.dropna(subset=['col1'])
+        exp = exp.reset_index(drop=True)
+        exp['col1'] = exp['col1'].astype('int64')
+        assert_frame_equal(res, exp)
+        assert_geoseries_equal(res.geometry, exp.geometry)
+
+    def test_nondefault_index(self):
+        df1 = self.df1.copy()
+        df1.index = ['row1', 'row2']
+        res = overlay(df1, self.df2, how='intersection',
+                      use_sindex=self.use_sindex)
+        exp = self.result.dropna(subset=['col1', 'col2'], how='any')
+        exp = exp.reset_index(drop=True)
+        exp[['col1', 'col2']] = exp[['col1', 'col2']].astype('int64')
+        assert_frame_equal(res, exp)
+        assert_geoseries_equal(res.geometry, exp.geometry)
+
+
+class TestOverlayNoSIndex(TestOverlay):
+
+    use_sindex = False
