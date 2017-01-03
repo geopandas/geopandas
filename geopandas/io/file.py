fa8c93a22a2be94117f9ef01c7a53fd3c6416e1b
James McBride <jmcbride@berkeley.edu>
2017-01-03 15:50:39 -0800

    Move geometry to last column in read_file.

--- geopandas/io/file.py
+++ geopandas/io/file.py
@@ -14,7 +14,7 @@ def read_file(filename, **kwargs):
 
     *filename* is either the absolute or relative path to the file to be
     opened and *kwargs* are keyword args to be passed to the `open` method
-    in the fiona library when opening the file. For more information on 
+    in the fiona library when opening the file. For more information on
     possible keywords, type: ``import fiona; help(fiona.open)``
     """
     bbox = kwargs.pop('bbox', None)
@@ -27,8 +27,8 @@ def read_file(filename, **kwargs):
             f_filt = f
         gdf = GeoDataFrame.from_features(f_filt, crs=crs)
 
-        # re-order with column order from metadata, with geometry first
-        columns = ["geometry"] + list(f.meta["schema"]["properties"])
+        # re-order with column order from metadata, with geometry last
+        columns = list(f.meta["schema"]["properties"]) + ["geometry"]
         gdf = gdf[columns]
 
     return gdf
