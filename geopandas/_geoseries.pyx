4dc4ad684cb39eafaa0227bfee400effdb06ba59
Matthew Rocklin <mrocklin@gmail.com>
2017-07-15 18:07:42 -0500

    add joris' contains solution

--- geopandas/_geoseries.pyx
+++ geopandas/_geoseries.pyx
@@ -39,3 +39,39 @@ cdef _cy_series_op(this, other, op, kwargs):
     else:
         return Series([getattr(s, op)(other, **kwargs) if s else null_val
                       for s in this.geometry], index=this.index)
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+def contains_cy(array, geometry):
+
+    cdef Py_ssize_t idx
+    cdef unsigned int n = array.size
+    cdef np.ndarray[np.uint8_t, ndim=1, cast=True] result = np.empty(n, dtype=np.uint8)
+
+    cdef GEOSContextHandle_t geos_handle
+    cdef GEOSPreparedGeometry *geom1
+    cdef GEOSGeometry *geom2
+    cdef uintptr_t geos_geom
+
+    # TODO: take `op` like "contains" and replace `GEOSPreparedContains_r`
+    #       below with a switch statement on op.
+
+    # Prepare the geometry if it hasn't already been prepared.
+    if not isinstance(geometry, shapely.prepared.PreparedGeometry):
+        geometry = shapely.prepared.prep(geometry)
+
+    geos_h = get_geos_context_handle()
+    geom1 = geos_from_prepared(geometry)
+
+    for idx in xrange(n):
+        # Construct a coordinate sequence with our x, y values.
+        geos_geom = array[idx]._geom
+        geom2 = <GEOSGeometry *>geos_geom
+
+        # Put the result of whether the point is "contained" by the
+        # prepared geometry into the result array.
+        result[idx] = <np.uint8_t> GEOSPreparedContains_r(geos_h, geom1, geom2)
+        #GEOSGeom_destroy_r(geos_h, geom2)
+
+    return result.view(dtype=np.bool)
