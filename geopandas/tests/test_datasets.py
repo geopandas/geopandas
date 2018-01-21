38dd9b6dd6b68c2af5ef593842853016c98b6bba
cr458 <christopher.x.ren@gmail.com>
2018-01-21 23:00:26 +0000

    TST: removed unnecessary tester class in test_datasets.py

--- geopandas/tests/test_datasets.py
+++ geopandas/tests/test_datasets.py
@@ -3,16 +3,13 @@ from __future__ import absolute_import
 from geopandas import read_file, GeoDataFrame
 from geopandas.datasets import get_path
 
+def test_read_paths(self):
 
-class TestDatasets:
+    gdf = read_file(get_path('naturalearth_lowres'))
+    assert isinstance(gdf, GeoDataFrame)
 
-    def test_read_paths(self):
+    gdf = read_file(get_path('naturalearth_cities'))
+    assert isinstance(gdf, GeoDataFrame)
 
-        gdf = read_file(get_path('naturalearth_lowres'))
-        assert isinstance(gdf, GeoDataFrame)
-
-        gdf = read_file(get_path('naturalearth_cities'))
-        assert isinstance(gdf, GeoDataFrame)
-
-        gdf = read_file(get_path('nybb'))
-        assert isinstance(gdf, GeoDataFrame)
+    gdf = read_file(get_path('nybb'))
+    assert isinstance(gdf, GeoDataFrame)
