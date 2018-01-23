b39ebdfa4a04ab11b23022e41fff6e9e4cf19940
cr458 <christopher.x.ren@gmail.com>
2018-01-23 21:33:48 +0000

    parametrized test_datasets.py

--- geopandas/tests/test_datasets.py
+++ geopandas/tests/test_datasets.py
@@ -1,15 +1,13 @@
 from __future__ import absolute_import
 
+import pytest
+
 from geopandas import read_file, GeoDataFrame
 from geopandas.datasets import get_path
 
-def test_read_paths(self):
-
-    gdf = read_file(get_path('naturalearth_lowres'))
-    assert isinstance(gdf, GeoDataFrame)
-
-    gdf = read_file(get_path('naturalearth_cities'))
-    assert isinstance(gdf, GeoDataFrame)
-
-    gdf = read_file(get_path('nybb'))
-    assert isinstance(gdf, GeoDataFrame)
+@pytest.mark.parametrize("test_gdf",
+                         [read_file(get_path('naturalearth_lowres')),
+                          read_file(get_path('naturalearth_cities')),
+                          read_file(get_path('nybb'))])
+def test_read_paths(test_gdf):
+    assert isinstance(test_gdf, GeoDataFrame)
