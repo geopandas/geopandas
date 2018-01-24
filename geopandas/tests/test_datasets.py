5cd4a0c9c8c13f693fba9f923d4c367f71671462
cr458 <christopher.x.ren@gmail.com>
2018-01-24 22:29:20 +0000

    TST: moved read_file + get_path inside test

--- geopandas/tests/test_datasets.py
+++ geopandas/tests/test_datasets.py
@@ -5,9 +5,9 @@ import pytest
 from geopandas import read_file, GeoDataFrame
 from geopandas.datasets import get_path
 
-@pytest.mark.parametrize("test_gdf",
-                         [read_file(get_path('naturalearth_lowres')),
-                          read_file(get_path('naturalearth_cities')),
-                          read_file(get_path('nybb'))])
-def test_read_paths(test_gdf):
-    assert isinstance(test_gdf, GeoDataFrame)
+@pytest.mark.parametrize("test_dataset",
+                         ['naturalearth_lowres',
+                          'naturalearth_cities',
+                          'nybb'])
+def test_read_paths(test_dataset):
+    assert isinstance(read_file(get_path(test_dataset)), GeoDataFrame)
