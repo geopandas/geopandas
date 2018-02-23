4c6f1806aaf21592b18c03dd6c432638377d3628
James McBride <jdmcbr@gmail.com>
2018-02-22 22:06:58 -0800

    Add attempt at srid setting in to_postgis
    Not working at present for reasons not evident to me.

--- geopandas/io/sql.py
+++ geopandas/io/sql.py
@@ -1,5 +1,6 @@
 import binascii
 
+import fiona.crs
 from pandas import read_sql
 import shapely.wkb
 
@@ -49,7 +50,29 @@ def read_postgis(sql, con, geom_col='geom', crs=None, index_col=None,
     return GeoDataFrame(df, crs=crs, geometry=geom_col)
 
 
-def to_postgis(df, name, con, **kwargs):
+def get_srid(crs):
+    """
+    Returns the SRID from a CRS dict or string, if available.
+
+    Parameters
+    ----------
+    crs : dict or str
+
+    Returns
+    -------
+    srid : str
+        If unsuccessful, returns None.
+    """
+
+    if isinstance(crs, str):
+        crs = fiona.crs.from_string(crs)
+
+    if 'init' in crs.keys():
+        if 'epsg' in crs['init']:
+            return crs['init'].split('epsg:')[1]
+
+
+def to_postgis(df, name, con, hex_encoded=True, **kwargs):
     """
     Writes a geodataframe to a postgis database.
 
@@ -68,10 +91,17 @@ def to_postgis(df, name, con, **kwargs):
     temp_df = df.copy()
 
     def convert_geometry(geom):
-        return binascii.hexlify(shapely.wkb.dumps(geom)).decode()
+        return shapely.wkb.dumps(geom, hex=hex_encoded)
 
     temp_df[temp_df.geometry.name] = geom.apply(convert_geometry)
     temp_df.to_sql(name, con, **kwargs)
-    alter_command = "ALTER TABLE {name} ALTER COLUMN {geom_name} TYPE geometry;"
     alter_args = {"name": name, "geom_name": temp_df.geometry.name}
-    con.execute(alter_command.format(**alter_args))
+    alter_cmd = "ALTER TABLE {name} ALTER COLUMN {geom_name} TYPE geometry;"
+    con.execute(alter_cmd.format(**alter_args))
+
+    srid = get_srid(df.crs)
+    if srid is not None:
+        srid_cmd = "SELECT UpdateGeometrySRID('{name}', '{geom_name}', {srid});"
+        alter_args["srid"] = srid
+        print(srid_cmd.format(**alter_args))
+        con.execute(srid_cmd.format(**alter_args))
