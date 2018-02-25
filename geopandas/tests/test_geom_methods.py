0d59440a3e0ca33d3e882255d3dac55e7e50e305
cr458 <christopher.x.ren@gmail.com>
2018-02-25 19:41:37 +0000

    updated test for Geoseries explode to include changes on Index names

--- geopandas/tests/test_geom_methods.py
+++ geopandas/tests/test_geom_methods.py
@@ -482,16 +482,16 @@ class TestGeomMethods:
     def test_explode_geoseries(self):
         s = GeoSeries([MultiPoint([(0, 0), (1, 1)]),
                        MultiPoint([(2, 2), (3, 3), (4, 4)])])
-
+        s.index.name = 'test_index_name'
+        expected_index_name = ['test_index_name', None]
         index = [(0, 0), (0, 1), (1, 0), (1, 1), (1, 2)]
         expected = GeoSeries([Point(0, 0), Point(1, 1), Point(2, 2),
                               Point(3, 3), Point(4, 4)],
-                             index=MultiIndex.from_tuples(index))
-
+                             index=MultiIndex.from_tuples(
+                                index, names=expected_index_name))
         assert_geoseries_equal(expected, s.explode())
 
     def test_explode_geodataframe(self):
-        import pdb
         s = GeoSeries([MultiPoint([Point(1, 2), Point(2, 3)]), Point(5, 5)])
         df = GeoDataFrame({'col': [1, 2], 'geometry': s})
         test_df = df.explode()
@@ -503,7 +503,6 @@ class TestGeomMethods:
             names=['level_0', 'level_1'])
 
         expected_df = expected_df.set_index(expected_index)
-        #pdb.set_trace()
         assert_frame_equal(test_df, expected_df)
 
     #
