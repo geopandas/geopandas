45a28e8fa0f6817d3e9813f7cc93f65230fce18a
Mehdi <rahim.mehdi@gmail.com>
2018-03-17 21:47:30 +0100

    add wkt parsing

--- examples/create_geopandas_from_pandas.py
+++ examples/create_geopandas_from_pandas.py
@@ -1,9 +1,12 @@
 """
-Creating a GeoDataFrame from a DataFrame
-----------------------------------------
+Creating a GeoDataFrame from a DataFrame with coordinates
+---------------------------------------------------------
 
 This example shows how to create a ``GeoDataFrame`` when starting from
-a *regular* ``DataFrame`` that has coordinates stored in two columns.
+a *regular* ``DataFrame`` that has coordinates either WKT
+(`well-known text <https://en.wikipedia.org/wiki/Well-known_text>`_)
+format, or in
+two columns.
 
 """
 import matplotlib.pyplot as plt
@@ -12,14 +15,17 @@ import geopandas as gpd
 from shapely.geometry import Point
 
 ###############################################################################
+# From longitudes and latitudes
+# =============================
+#
 # First, let's consider a ``DataFrame`` containing cities and their respective
 # longitudes and latitudes.
 
 df = pd.DataFrame(
     {'City': ['Buenos Aires', 'Brasilia', 'Santiago', 'Bogota', 'Caracas'],
      'Country': ['Argentina', 'Brazil', 'Chile', 'Colombia', 'Venezuela'],
-     'Latitude': [-34.583333, -15.783333, -33.450000, 4.600000, 10.483333],
-     'Longitude': [-58.666667, -47.916667, -70.666667, -74.083333, -66.866667]})
+     'Latitude': [-34.58, -15.78, -33.45, 4.60, 10.48],
+     'Longitude': [-58.66, -47.91, -70.66, -74.08, -66.86]})
 
 ###############################################################################
 # A ``GeoDataFrame`` needs a ``shapely`` object, so we create a new column
@@ -38,6 +44,10 @@ df['Coordinates'] = df['Coordinates'].apply(Point)
 
 gdf = gpd.GeoDataFrame(df, geometry='Coordinates')
 
+###############################################################################
+# ``gdf`` looks like this :
+
+print(gdf.head())
 
 ###############################################################################
 # Finally, we plot the coordinates over a country-level map.
@@ -52,3 +62,28 @@ ax = world[world.continent == 'South America'].plot(
 gdf.plot(ax=ax, color='red')
 
 plt.show()
+
+###############################################################################
+# From WKT format
+# ===============
+# Here, we consider a ``DataFrame`` having coordinates in WKT format.
+
+df = pd.DataFrame(
+    {'City': ['Buenos Aires', 'Brasilia', 'Santiago', 'Bogota', 'Caracas'],
+     'Country': ['Argentina', 'Brazil', 'Chile', 'Colombia', 'Venezuela'],
+     'Coordinates': ['POINT(-34.58 -58.66)', 'POINT(-15.78 -47.91)',
+                     'POINT(-33.45 -70.66)', 'POINT(4.60 -74.08)',
+                     'POINT(10.48 -66.86)']})
+
+###############################################################################
+# We use ``shapely.wkt`` sub-module to parse wkt format:
+from shapely import wkt
+
+df['Coordinates'] = df['Coordinates'].apply(wkt.loads)
+
+###############################################################################
+#  The ``GeoDataFrame`` is constructed as follows :
+
+gdf = gpd.GeoDataFrame(df, geometry='Coordinates')
+
+print(gdf.head())
