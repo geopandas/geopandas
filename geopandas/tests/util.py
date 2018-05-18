6e0fd9b18e5e992434143e79db72c20104e9f3ef
James McBride <jdmcbr@gmail.com>
2018-05-17 22:57:21 -0700

    ENH: get SRID from postgis read (#627)
    * ENH: get SRID from postgis read
    
    As currently implemented, this is relying on string parsing from the sql
    string passed in by the user. An API change could achieve this without
    the string parsing. Or something else I'm not seeing?
    
    * Fix polygons -> geometries in comment
    
    * Remove brittle SRID postgis extraction
    
    Change previous test to xfail, add test of "select geom as the_geom".
    
    * Use GEOSGetSRID to return SRID from postgis
    
    Per @jorisvandenbossche suggestion. Also simplifies wkb loading using
    the shapely.skb.loads argument hex=True. Add the argument hex_encoded to
    read_postgis so that non hex geometries might also be read.
    
    * Un-xfail test for SRID read
    
    With SRID read from geodatabase support, can now set CRS automatically
    so that user does not need to.
    
    * Remove check for unique SRID in sql read
    
    Assume the CRS for frame from SRID in first geometry.
    
    * Add test of sqlite database read
    
    * Add type check in sql geometry read
    
    I think this both addresses python2 failures for sqlite and WKBElement
    reads.
    
    * Rename read_postgis to read_sql
    
    Create wrappers read_postgis and read_sqlite
    
    * Add read_sqlite to __init__
    
    * Add test to create sqlite database for testing
    
    Rename the function for postgis from create_db to create_postgis. This
    permits not shipping the sqlite table as part of the datasets.
    
    * Amend read_sql docs per Joris's comments
    
    * Remove nybb.sqlite dataset
    
    * Clean up sqlite db creation/execution
    
    * Remove explicit sqlite support
    
    * Remove read_sqlite command from __init__ too
    
    * Fix sqlite read tests
    
    * Fix failures in merge conflict resolution

--- geopandas/tests/util.py
+++ geopandas/tests/util.py
@@ -1,5 +1,7 @@
 import os.path
+import sqlite3
 
+import shapely.wkb
 from geopandas import GeoDataFrame
 from geopandas.testing import (
     geom_equals, geom_almost_equals, assert_geoseries_equal)  # flake8: noqa
@@ -15,6 +17,7 @@ except ImportError:
     class OperationalError(Exception):
         pass
 
+# mock not used here, but the import from here is used in other modules
 try:
     import unittest.mock as mock
 except ImportError:
@@ -22,7 +25,7 @@ except ImportError:
 
 
 def validate_boro_df(df, case_sensitive=False):
-    """ Tests a GeoDataFrame that has been read in from the nybb dataset."""
+    """Tests a GeoDataFrame that has been read in from the nybb dataset."""
     assert isinstance(df, GeoDataFrame)
     # Make sure all the columns are there and the geometries
     # were properly loaded as MultiPolygons
@@ -46,12 +49,36 @@ def connect(dbname):
     return con
 
 
-def create_db(df):
+def create_sqlite(df, filename, geom_col="geom"):
+    """
+    Create a sqlite database with the nybb table. This was the result of
+    using ogr2ogr to create a sqlite database from a shapefile, and then
+    the .dump command within sqlite to produce the commands to reproduce the
+    database, so may not representative of standard sqlite databases.
+    """
+
+    con = sqlite3.connect(filename)
+    cur = con.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS 'nybb' "
+                "( ogc_fid INTEGER PRIMARY KEY AUTOINCREMENT, "
+                "'{}' BLOB, 'borocode' INTEGER, ".format(geom_col) +
+                "'boroname' VARCHAR(32), 'shape_leng' FLOAT, "
+                "'shape_area' FLOAT);")
+    sql_row = "INSERT INTO nybb VALUES({},X'{}',{},'{}',{},{});"
+    for i, row in df.iterrows():
+        cur.execute(sql_row.format(i, shapely.wkb.dumps(row['geometry'],
+                                                        hex=True),
+                                   row['BoroCode'], row['BoroName'],
+                                   row['Shape_Leng'], row['Shape_Area']))
+    con.commit()
+    con.close()
+
+
+def create_postgis(df, srid=None, geom_col="geom"):
     """
     Create a nybb table in the test_geopandas PostGIS database.
     Returns a boolean indicating whether the database table was successfully
     created
-
     """
     # Try to create the database, skip the db tests if something goes
     # wrong
@@ -63,23 +90,28 @@ def create_db(df):
     if con is None:
         return False
 
+    if srid is not None:
+        geom_schema = "geometry(MULTIPOLYGON, {})".format(srid)
+        geom_insert = ("ST_SetSRID(ST_GeometryFromText(%s), {})".format(srid))
+    else:
+        geom_schema = "geometry"
+        geom_insert = "ST_GeometryFromText(%s)"
     try:
         cursor = con.cursor()
         cursor.execute("DROP TABLE IF EXISTS nybb;")
 
         sql = """CREATE TABLE nybb (
-            geom        geometry,
-            borocode    integer,
-            boroname    varchar(40),
-            shape_leng  float,
-            shape_area  float
-        );"""
+            {geom_col}   {geom_schema},
+            borocode     integer,
+            boroname     varchar(40),
+            shape_leng   float,
+            shape_area   float
+            );""".format(geom_col=geom_col, geom_schema=geom_schema)
         cursor.execute(sql)
 
         for i, row in df.iterrows():
-            sql = """INSERT INTO nybb VALUES (
-                ST_GeometryFromText(%s), %s, %s, %s, %s
-            );"""
+            sql = """INSERT INTO nybb VALUES ({}, %s, %s, %s, %s
+            );""".format(geom_insert)
             cursor.execute(sql, (row['geometry'].wkt,
                                  row['BoroCode'],
                                  row['BoroName'],
@@ -91,5 +123,3 @@ def create_db(df):
         con.close()
 
     return True
-
-
