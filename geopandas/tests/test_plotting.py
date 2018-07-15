abbb38ebff387899721b8846e3e681155b247999
pinto531 <pinto531@yahoo.ca>
2018-07-15 12:18:26 -0500

    ENH: pass array/series of values to column argument in plot (#770)

--- geopandas/tests/test_plotting.py
+++ geopandas/tests/test_plotting.py
@@ -632,6 +632,42 @@ class TestPlotCollections:
         ax.cla()
 
 
+def test_column_values():
+    """
+    Check that the dataframe plot method returns same values with an
+    input string (column in df), pd.Series, or np.array
+    """
+    # Build test data
+    t1 = Polygon([(0, 0), (1, 0), (1, 1)])
+    t2 = Polygon([(1, 0), (2, 0), (2, 1)])
+    polys = GeoSeries([t1, t2], index=list('AB'))
+    df = GeoDataFrame({'geometry': polys, 'values': [0, 1]})
+
+    # Test with continous values
+    ax = df.plot(column='values')
+    colors = ax.collections[0].get_facecolors()
+    ax = df.plot(column=df['values'])
+    colors_series = ax.collections[0].get_facecolors()
+    np.testing.assert_array_equal(colors, colors_series)
+    ax = df.plot(column=df['values'].values)
+    colors_array = ax.collections[0].get_facecolors()
+    np.testing.assert_array_equal(colors, colors_array)
+
+    # Test with categorical values
+    ax = df.plot(column='values', categorical=True)
+    colors = ax.collections[0].get_facecolors()
+    ax = df.plot(column=df['values'], categorical=True)
+    colors_series = ax.collections[0].get_facecolors()
+    np.testing.assert_array_equal(colors, colors_series)
+    ax = df.plot(column=df['values'].values, categorical=True)
+    colors_array = ax.collections[0].get_facecolors()
+    np.testing.assert_array_equal(colors, colors_array)
+
+    # Check raised error: is df rows number equal to column legth?
+    with pytest.raises(ValueError, match="different number of rows"):
+        ax = df.plot(column=np.array([1, 2, 3]))
+
+
 def _check_colors(N, actual_colors, expected_colors, alpha=None):
     """
     Asserts that the members of `collection` match the `expected_colors`
