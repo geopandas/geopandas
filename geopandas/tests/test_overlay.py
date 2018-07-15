2d41edb27135e28584e94bdb5d5614eedb9ecda8
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2018-07-15 16:28:05 -0500

    Fix warnings with overlay and pandas >= 0.23 (#772)

--- geopandas/tests/test_overlay.py
+++ geopandas/tests/test_overlay.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 
 import os
+from distutils.version import LooseVersion
 
 import pandas as pd
 from shapely.geometry import Point, Polygon
@@ -14,6 +15,12 @@ import pytest
 
 DATA = os.path.join(
     os.path.abspath(os.path.dirname(__file__)), 'data', 'overlay')
+
+
+if str(pd.__version__) < LooseVersion('0.23'):
+    CONCAT_KWARGS = {}
+else:
+    CONCAT_KWARGS = {'sort': False}
 
 
 @pytest.fixture
@@ -72,7 +79,7 @@ def test_overlay(dfs_index, how, use_sindex):
         expected = pd.concat([
             expected_intersection,
             expected_difference
-        ], ignore_index=True)
+        ], ignore_index=True, **CONCAT_KWARGS)
     else:
         expected = _read(how)
 
