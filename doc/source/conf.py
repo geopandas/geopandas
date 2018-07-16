394c06a56cacb7c94c737b8faed31b1c0e92774f
James McBride <jdmcbr@gmail.com>
2018-07-15 21:50:33 -0700

    DOC: update copyright date range

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -70,7 +70,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'GeoPandas'
-copyright = u'2013–2017, GeoPandas developers'
+copyright = u'2013–2018, GeoPandas developers'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
