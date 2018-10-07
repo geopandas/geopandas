6d0cec500e79d25cad796e0d1b041632cf354b2c
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2018-10-07 17:01:50 +0200

    add test

--- geopandas/tests/test_testing.py
+++ geopandas/tests/test_testing.py
@@ -33,3 +33,8 @@ def test_geodataframe():
         assert_geodataframe_equal(df1, df2[['geometry', 'col1']])
 
     assert_geodataframe_equal(df1, df2[['geometry', 'col1']], check_like=True)
+
+    df3 = df2.copy()
+    df3.loc[0, 'col1'] = 10
+    with pytest.raises(AssertionError):
+        assert_geodataframe_equal(df1, df2)
