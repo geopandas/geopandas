d48fecee6351033f6316040c16193912e877867a
Ömer Özak <ozak@users.noreply.github.com>
2018-12-21 13:13:59 -0600

    Correct index for results

--- geopandas/tools/overlay.py
+++ geopandas/tools/overlay.py
@@ -372,7 +372,6 @@ def overlay(df1, df2, how='intersection', make_valid=True, use_sindex=None):
     elif how == 'identity':
         dfunion = _overlay_union(df1, df2)
         result = dfunion[dfunion['__idx1'].notnull()].copy()
-        result.reset_index(drop=True, inplace=True)
-    if set(result.columns).intersection(['__idx1', '__idx2'])!=set():
-        result.drop(['__idx1', '__idx2'], axis=1, inplace=True)
+    result.reset_index(drop=True, inplace=True)
+    result.drop(['__idx1', '__idx2'], axis=1, inplace=True)
     return result
