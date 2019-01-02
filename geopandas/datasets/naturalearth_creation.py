a935266e413435d29ca4a96f90ceaaf5435fdf8e
James McBride <jdmcbr@gmail.com>
2019-01-01 21:09:16 -0800

    Make small revisions to demo dataset creation file

--- geopandas/datasets/naturalearth_creation.py
+++ geopandas/datasets/naturalearth_creation.py
@@ -3,11 +3,15 @@ Script that generates the included dataset 'naturalearth_lowres.shp'.
 
 Raw data: https://www.naturalearthdata.com/downloads/10m-cultural-vectors/10m-admin-0-countries/
 Current version used: version 4.1.0
-
 """
+
 import geopandas as gpd
-world_raw = gpd.read_file("zip://./ne_110m_admin_0_countries.zip") #reads the zip file from https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_admin_0_countries.zip 
-#current version 4.1.0
-select_columns = world_raw[['POP_EST', 'CONTINENT', 'NAME', 'ISO_A3', 'GDP_MD_EST', 'geometry']] #selects the columns used in the example dataset and writes them to the example dataset
-select_columns.columns = select_columns.columns.str.lower() #changes column names to lowercase
-select_columns.to_file(driver='ESRI Shapefile', filename='./naturalearth_lowres/naturalearth_lowres.shp') #writes the data as a shapefile
+
+# assumes zipfile from naturalearthdata was downloaded to current directory
+world_raw = gpd.read_file("zip://./ne_110m_admin_0_countries.zip")
+# subsets columns of interest for geopandas examples
+world_df = world_raw[['POP_EST', 'CONTINENT', 'NAME', 'ISO_A3',
+                      'GDP_MD_EST', 'geometry']]
+world_df.columns = world_df.columns.str.lower()
+world_df.to_file(driver='ESRI Shapefile',
+                 filename='./naturalearth_lowres/naturalearth_lowres.shp')
