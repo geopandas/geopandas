1845ceb1d0fed8e4e88529e2890636b017e8a45f
Ömer Özak <ozak@users.noreply.github.com>
2019-01-03 10:35:27 -0600

    Added test

--- geopandas/tests/test_overlay.py
+++ geopandas/tests/test_overlay.py
@@ -303,3 +303,15 @@ def test_empty_intersection(dfs):
     expected = GeoDataFrame([], columns=['col1', 'col3', 'geometry'])
     result = overlay(df1, df3)
     assert_geodataframe_equal(result, expected, check_like=True)
+
+def test_index(dfs):
+    df1, df2 = dfs
+    polys3 = GeoSeries([Polygon([(1,1), (3,1), (3,3), (1,3)]),
+                            Polygon([(-1,1), (1,1), (1,3), (-1,3)]),
+                            Polygon([(3,3), (5,3), (5,5), (3,5)])])
+    df3 = GeoDataFrame({'geometry': polys3, 'col3':[1,2,3]})
+    i1 = Polygon([(1,1), (1,3), (3,3), (3,1), (1,1)])
+    i2 = Polygon([(3,3), (3,5), (5,5), (5,3), (3,3)])
+    expected = GeoDataFrame([[1, 1, i1], [3, 2, i2]], columns=['col3', 'col2', 'geometry'])
+    result = overlay(df3, df2)
+    assert_geodataframe_equal(result, expected)
