bba9ad0b65f48e2f02468ff0f2c03148a76885da
Ryan McCarthy <ryan.mccarthy@weyerhaeuser.com>
2019-03-20 15:49:39 -0700

    adding type check to handle numpy datetime64 types

--- geopandas/io/file.py
+++ geopandas/io/file.py
@@ -132,6 +132,8 @@ def infer_schema(df):
     def convert_type(column, in_type):
         if in_type == object:
             return 'str'
+        if hasattr(in_type, 'type') and in_type.type == np.datetime64:
+            return 'datetime'
         out_type = type(np.zeros(1, in_type).item()).__name__
         if out_type == 'long':
             out_type = 'int'
