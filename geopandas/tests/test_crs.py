8707de8629ce8b1f68e442c673148e1076b0c0c8
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2019-04-15 14:26:25 +0200

    clean-up existing tests (no setup, helper function to set up dataframe

--- geopandas/tests/test_crs.py
+++ geopandas/tests/test_crs.py
@@ -1,41 +1,41 @@
-from shapely.geometry import Point
+import numpy as np
 
-from geopandas import GeoDataFrame
+from geopandas import GeoDataFrame, points_from_xy
+
+
+def _create_df(x, y=None, crs=None):
+    y = y or x
+    x = np.asarray(x)
+    y = np.asarray(y)
+
+    return GeoDataFrame(
+        {'geometry': points_from_xy(x, y), 'value1': x + y, 'value2': x * y},
+        crs=crs)
 
 
 class TestToCRS:
 
-    def setup_method(self):
-        N = 10
-        self.crs = {'init': 'epsg:4326'}
-        self.df2 = GeoDataFrame([
-            {'geometry': Point(x, y), 'value1': x + y, 'value2': x * y}
-            for x, y in zip(range(N), range(N))], crs=self.crs)
-
     def test_transform(self):
-        df2 = self.df2.copy()
-        df2.crs = {'init': 'epsg:26918', 'no_defs': True}
-        lonlat = df2.to_crs(epsg=4326)
+        df = _create_df(range(10), crs={'init': 'epsg:26918', 'no_defs': True})
+        lonlat = df.to_crs(epsg=4326)
         utm = lonlat.to_crs(epsg=26918)
-        assert all(df2['geometry'].geom_almost_equals(utm['geometry'],
-                                                      decimal=2))
+        assert all(df['geometry'].geom_almost_equals(utm['geometry'],
+                                                     decimal=))
 
     def test_transform_inplace(self):
-        df2 = self.df2.copy()
-        df2.crs = {'init': 'epsg:26918', 'no_defs': True}
-        lonlat = df2.to_crs(epsg=4326)
-        df2.to_crs(epsg=4326, inplace=True)
-        assert all(df2['geometry'].geom_almost_equals(lonlat['geometry'],
-                                                      decimal=2))
+        df = _create_df(range(10), crs={'init': 'epsg:26918', 'no_defs': True})
+        lonlat = df.to_crs(epsg=4326)
+        df.to_crs(epsg=4326, inplace=True)
+        assert all(df['geometry'].geom_almost_equals(lonlat['geometry'],
+                                                     decimal=2))
 
     def test_to_crs_geo_column_name(self):
         # Test to_crs() with different geometry column name (GH#339)
-        df2 = self.df2.copy()
-        df2.crs = {'init': 'epsg:26918', 'no_defs': True}
-        df2 = df2.rename(columns={'geometry': 'geom'})
-        df2.set_geometry('geom', inplace=True)
-        lonlat = df2.to_crs(epsg=4326)
+        df = _create_df(range(10), crs={'init': 'epsg:26918', 'no_defs': True})
+        df = df.rename(columns={'geometry': 'geom'})
+        df.set_geometry('geom', inplace=True)
+        lonlat = df.to_crs(epsg=4326)
         utm = lonlat.to_crs(epsg=26918)
         assert lonlat.geometry.name == 'geom'
         assert utm.geometry.name == 'geom'
-        assert all(df2.geometry.geom_almost_equals(utm.geometry, decimal=2))
+        assert all(df.geometry.geom_almost_equals(utm.geometry, decimal=2))
