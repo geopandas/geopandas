19a4291661516ef59178e472843c3e367806ef8d
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2019-04-15 15:08:57 +0200

    Use coordinates within bounds of the specific used crs (EPSG:26918)

--- geopandas/tests/test_crs.py
+++ geopandas/tests/test_crs.py
@@ -14,23 +14,32 @@ def _create_df(x, y=None, crs=None):
         crs=crs)
 
 
+def df_epsg26918():
+    # EPSG:26918
+    # Center coordinates
+    # -1683723.64 6689139.23
+    return _create_df(x=range(-1683723, -1683723 + 10, 1),
+                      y=range(6689139, 6689139 + 10, 1),
+                      crs={'init': 'epsg:26918', 'no_defs': True})
+
+
 class TestToCRS:
 
     def test_transform(self):
-        df = _create_df(range(10), crs={'init': 'epsg:26918', 'no_defs': True})
+        df = df_epsg26918()
         lonlat = df.to_crs(epsg=4326)
         utm = lonlat.to_crs(epsg=26918)
         assert_geodataframe_equal(df, utm, check_less_precise=True)
 
     def test_transform_inplace(self):
-        df = _create_df(range(10), crs={'init': 'epsg:26918', 'no_defs': True})
+        df = df_epsg26918()
         lonlat = df.to_crs(epsg=4326)
         df.to_crs(epsg=4326, inplace=True)
         assert_geodataframe_equal(df, lonlat, check_less_precise=True)
 
     def test_to_crs_geo_column_name(self):
         # Test to_crs() with different geometry column name (GH#339)
-        df = _create_df(range(10), crs={'init': 'epsg:26918', 'no_defs': True})
+        df = df_epsg26918()
         df = df.rename(columns={'geometry': 'geom'})
         df.set_geometry('geom', inplace=True)
         lonlat = df.to_crs(epsg=4326)
