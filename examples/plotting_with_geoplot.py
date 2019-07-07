0baed88ff2ee94b5cea54d07f8b0c1bf10e1aa2b
ResidentMario <aleksey.bilogur@gmail.com>
2019-07-07 14:40:12 -0400

    Updates per comments.

--- examples/plotting_with_geoplot.py
+++ examples/plotting_with_geoplot.py
@@ -14,19 +14,19 @@ library refer to `its documentation
 
 First we'll load in the data using GeoPandas.
 """
-import geopandas as gpd
-import geoplot as gplt
+import geopandas
+import geoplot
 
-world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
-boroughs = gpd.read_file(gplt.datasets.get_path('nyc_boroughs'))
-collisions = gpd.read_file(gplt.datasets.get_path('nyc_injurious_collisions'))
+world = geopandas.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+boroughs = geopandas.read_file(gplt.datasets.get_path('nyc_boroughs'))
+collisions = geopandas.read_file(gplt.datasets.get_path('nyc_injurious_collisions'))
 
 ###############################################################################
 # Plotting with Geoplot
 # =====================
 #
 # We start out by replicating the basic GeoPandas world plot using Geoplot.
-gplt.polyplot(world, figsize=(8, 4))
+geoplot.polyplot(world, figsize=(8, 4))
 
 ###############################################################################
 # Geoplot can re-project data into any of the map projections provided by
@@ -34,7 +34,8 @@ gplt.polyplot(world, figsize=(8, 4))
 # `here <http://scitools.org.uk/cartopy/docs/latest/crs/projections.html>`_).
 
 import geoplot.crs as gcrs
-ax = gplt.polyplot(world, projection=gcrs.Orthographic(), figsize=(8, 4))
+# use the Orthographic map projection (e.g. a world globe)
+ax = geoplot.polyplot(world, projection=gcrs.Orthographic(), figsize=(8, 4))
 ax.outline_patch.set_visible(True)
 
 ###############################################################################
@@ -42,7 +43,7 @@ ax.outline_patch.set_visible(True)
 # you want to use color as a visual variable, specify a ``choropleth``. Here
 # we sort GDP per person by country into five buckets by color.
 
-gplt.choropleth(
+geoplot.choropleth(
     world, hue=world['gdp_md_est'] / world['pop_est'],
     cmap='Greens', figsize=(8, 4)
 )
@@ -52,39 +53,39 @@ gplt.choropleth(
 # population estimates for countries in Africa.
 
 africa = world.query('continent == "Africa"')
-ax = gplt.cartogram(
+ax = geoplot.cartogram(
     africa, scale='pop_est', limits=(0.2, 1),
     edgecolor='None', figsize=(7, 8)
 )
-gplt.polyplot(africa, edgecolor='gray', ax=ax)
+geoplot.polyplot(africa, edgecolor='gray', ax=ax)
 
 ###############################################################################
 # If we have data in the shape of points in space, we may generate a
 # three-dimensional heatmap on it using ``kdeplot``.
 
-ax = gplt.kdeplot(
+ax = geoplot.kdeplot(
     collisions, clip=boroughs.geometry,
     shade=True, cmap='Reds',
     projection=gcrs.AlbersEqualArea())
-gplt.polyplot(boroughs, ax=ax, zorder=1)
+geoplot.polyplot(boroughs, ax=ax, zorder=1)
 
 ###############################################################################
 # Alternatively, we may partition the space into neighborhoods automatically,
 # using Voronoi tessellation. This is a good way of visually verifying whether
 # or not a certain data column is spatially correlated.
 
-ax = gplt.voronoi(
+ax = geoplot.voronoi(
     collisions.head(1000), projection=gcrs.AlbersEqualArea(),
     clip=boroughs.simplify(0.001),
     hue='NUMBER OF PERSONS INJURED', cmap='Reds', k=None,
     legend=True,
     edgecolor='white'
 )
-gplt.polyplot(boroughs, edgecolor='black', zorder=1, ax=ax)
+geoplot.polyplot(boroughs, edgecolor='black', zorder=1, ax=ax)
 
 ###############################################################################
 # These are just some of the plots you can make with Geoplot. There are
 # many other possibilities not covered in this brief introduction. For more
 # examples, refer to the
-# `Gallery <https://residentmario.github.io/geoplot/gallery.html>`_ in the
-# Geoplot documentation.
+# `Gallery <https://residentmario.github.io/geoplot/gallery/index.html>`_ in
+# the Geoplot documentation.
