fec1de436c693c74c3415596852aeab4bf482e71
Martin Fleischmann <martin@martinfleischmann.net>
2019-08-11 23:32:56 +0100

    tests

--- geopandas/tests/test_plotting.py
+++ geopandas/tests/test_plotting.py
@@ -345,6 +345,20 @@ class TestPolygonPlotting:
         expected_colors = [cmap(0), cmap(0), cmap(1), cmap(1)]
         _check_colors(4, ax.collections[0].get_facecolors(), expected_colors)
 
+    def test_subplots_norm(self):
+
+        cmap = matplotlib.cm.viridis_r
+        norm = matplotlib.colors.Normalize(vmin=0, vmax=10)
+        ax = self.df.plot(column='values', cmap=cmap, norm=norm)
+        actual_colors_orig = ax.collections[0].get_facecolors()
+        exp_colors = cmap(np.arange(2) / (10))
+        np.testing.assert_array_equal(exp_colors, actual_colors_orig)
+        fig, ax = plt.subplots()
+        self.df[1:].plot(column='values', ax=ax, norm=norm, cmap=cmap)
+        actual_colors_sub = ax.collections[0].get_facecolors()
+        np.testing.assert_array_equal(actual_colors_orig[1],
+                                      actual_colors_sub[0])
+
 
 class TestPolygonZPlotting:
 
