6d555d9db3ecb6403c924f644a406790430caae4
Thomas Pinder <tompinder@live.co.uk>
2019-08-12 11:49:18 +0100

    ENH: show_versions added (#918)
    * show_versions added
    
    * Redo based on sklearn
    
    * Added optional deps
    
    * optional to tests
    
    * fix old mapclassify

---
+++ geopandas/tools/_show_versions.py
@@ -0,0 +1,91 @@
+import platform
+import sys
+import importlib
+
+
+def _get_sys_info():
+    """System information
+
+    Returns
+    -------
+    sys_info : dict
+        system and Python version information
+    """
+    python = sys.version.replace('\n', ' ')
+
+    blob = [
+        ("python", python),
+        ('executable', sys.executable),
+        ("machine", platform.platform()),
+    ]
+
+    return dict(blob)
+
+
+def _get_deps_info():
+    """Overview of the installed version of main dependencies
+
+    Returns
+    -------
+    deps_info: dict
+        version information on relevant Python libraries
+    """
+    deps = [
+        "geopandas",
+        "pandas",
+        "fiona",
+        "osgeo.gdal",
+        "numpy",
+        "shapely",
+        "rtree",
+        "pyproj",
+        "matplotlib",
+        "mapclassify",
+        "pysal",
+        "geopy",
+        "psycopg2",
+        "descartes"
+    ]
+
+    def get_version(module):
+        return module.__version__
+
+    deps_info = {}
+
+    for modname in deps:
+        try:
+            if modname in sys.modules:
+                mod = sys.modules[modname]
+            else:
+                mod = importlib.import_module(modname)
+            ver = get_version(mod)
+            deps_info[modname] = ver
+        except ImportError:
+            deps_info[modname] = None
+        except AttributeError:
+            deps_info[modname] = None
+
+    return deps_info
+
+
+def show_versions():
+    """
+    Print system information and installed module versions.
+
+    Example
+    -------
+    > python -c "import geopandas; geopandas.show_versions()"
+    """
+    sys_info = _get_sys_info()
+    deps_info = _get_deps_info()
+
+    maxlen = max(len(x) for x in deps_info)
+    tpl = "{{k:<{maxlen}}}: {{stat}}".format(maxlen=maxlen)
+    print("\nSYSTEM INFO")
+    print("-----------")
+    for k, stat in sys_info.items():
+        print(tpl.format(k=k, stat=stat))
+    print("\nPYTHON DEPENDENCIES")
+    print("-------------------")
+    for k, stat in deps_info.items():
+        print(tpl.format(k=k, stat=stat))
