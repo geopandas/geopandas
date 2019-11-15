55da15546edebe6ba9c1111a4691cdedfbba1673
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2019-11-15 11:31:27 +0100

    TST: fix api test (no import) for matplotlib (#1202)
    (cherry picked from commit 37c37812e97991c9495b81cbf62e0d5efbfb9526)

--- geopandas/_compat.py
+++ geopandas/_compat.py
@@ -9,6 +9,7 @@ import six
 
 PANDAS_GE_024 = str(pd.__version__) >= LooseVersion("0.24.0")
 PANDAS_GE_025 = str(pd.__version__) >= LooseVersion("0.25.0")
+PANDAS_GE_10 = str(pd.__version__) >= LooseVersion("0.26.0.dev")
 
 
 # -----------------------------------------------------------------------------
