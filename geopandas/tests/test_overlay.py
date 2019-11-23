3794293ca853b9f8c52fecc4e9d3c8600fef0cc4
Martin Fleischmann <martin@martinfleischmann.net>
2019-11-23 17:10:46 +0000

    more tests

--- geopandas/tests/test_overlay.py
+++ geopandas/tests/test_overlay.py
@@ -329,8 +329,54 @@ def test_correct_index(dfs):
     assert_geodataframe_equal(result, expected)
 
 
-@pytest.mark.parametrize("geom_types", ["polys", "poly_line", "poly_point"])
+@pytest.mark.parametrize(
+    "geom_types", ["polys", "poly_line", "poly_point", "line_poly", "point_poly"]
+)
 def test_overlay_strict(how, strict, geom_types):
+    """
+    Test of mixed geometry types on input and output. Expected results initially
+    generated using following snippet.
+
+        polys1 = gpd.GeoSeries([Polygon([(1, 1), (3, 1), (3, 3), (1, 3)]),
+                                Polygon([(3, 3), (5, 3), (5, 5), (3, 5)])])
+        df1 = gpd.GeoDataFrame({'col1': [1, 2], 'geometry': polys1})
+
+        polys2 = gpd.GeoSeries([Polygon([(1, 1), (3, 1), (3, 3), (1, 3)]),
+                                Polygon([(-1, 1), (1, 1), (1, 3), (-1, 3)]),
+                                Polygon([(3, 3), (5, 3), (5, 5), (3, 5)])])
+        df2 = gpd.GeoDataFrame({'geometry': polys2, 'col2': [1, 2, 3]})
+
+        lines1 = gpd.GeoSeries([LineString([(2, 0), (2, 4), (6, 4)]),
+                                LineString([(0, 3), (6, 3)])])
+        df3 = gpd.GeoDataFrame({'col3': [1, 2], 'geometry': lines1})
+        points1 = gpd.GeoSeries([Point((2, 2)),
+                                 Point((3, 3))])
+        df4 = gpd.GeoDataFrame({'col4': [1, 2], 'geometry': points1})
+
+        params=["union", "intersection", "difference", "symmetric_difference",
+                "identity"]
+        stricts = [True, False]
+
+        for p in params:
+            for s in stricts:
+                exp = gpd.overlay(df1, df2, how=p, strict=s)
+                if not exp.empty:
+                    exp.to_file('polys_{p}_{s}.geojson'.format(p=p, s=s),
+                                driver='GeoJSON')
+
+        for p in params:
+            for s in stricts:
+                exp = gpd.overlay(df1, df3, how=p, strict=s)
+                if not exp.empty:
+                    exp.to_file('poly_line_{p}_{s}.geojson'.format(p=p, s=s),
+                                driver='GeoJSON')
+        for p in params:
+            for s in stricts:
+                exp = gpd.overlay(df1, df4, how=p, strict=s)
+                if not exp.empty:
+                    exp.to_file('poly_point_{p}_{s}.geojson'.format(p=p, s=s),
+                                driver='GeoJSON')
+    """
     polys1 = GeoSeries(
         [
             Polygon([(1, 1), (3, 1), (3, 3), (1, 3)]),
@@ -360,6 +406,10 @@ def test_overlay_strict(how, strict, geom_types):
         result = overlay(df1, df3, how=how, strict=strict)
     elif geom_types == "poly_point":
         result = overlay(df1, df4, how=how, strict=strict)
+    elif geom_types == "line_poly":
+        result = overlay(df3, df1, how=how, strict=strict)
+    elif geom_types == "point_poly":
+        result = overlay(df4, df1, how=how, strict=strict)
 
     if not _FIONA18 and result.empty:
         raise pytest.skip()
