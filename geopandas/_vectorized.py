1013ee8b09e056ddf1ea6239536a000483878ea0
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2020-02-19 13:52:14 +0100

    Use vectorized transform for to_crs

--- geopandas/_vectorized.py
+++ geopandas/_vectorized.py
@@ -829,3 +829,26 @@ def bounds(data):
         ]
     )
     return bounds
+
+
+#
+# Coordinate transformation
+#
+
+
+def transform(data, func):
+    if USE_PYGEOS:
+        coords = pygeos.get_coordinates(data)
+        new_coords = func(coords[:, 0], coords[:, 1])
+        result = pygeos.set_coordinates(data.copy(), np.array(new_coords).T)
+        return result
+    else:
+        from shapely.ops import transform
+
+        n = len(data)
+        result = np.empty(n, dtype=object)
+        for i in range(n):
+            geom = data[i]
+            result[i] = transform(func, geom)
+
+        return result
