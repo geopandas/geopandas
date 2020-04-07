d21697b7ff78442aead0cf49922aa8c433ed2e6a
Bo-Deng <boyang.deng@gmail.com>
2020-04-07 18:01:36 -0500

    Update read_postgis docstring for CRS changes
    Fixes issue #1350 "DOC: update read_postgis docstring for CRS changes"

--- geopandas/io/sql.py
+++ geopandas/io/sql.py
@@ -30,10 +30,27 @@ def read_postgis(
         Active connection to the database to query.
     geom_col : string, default 'geom'
         column name to convert to shapely geometries
-    crs : dict or str, optional
-        CRS to use for the returned GeoDataFrame; if not set, tries to
-        determine CRS from the SRID associated with the first geometry in
-        the database, and assigns that to all geometries.
+    crs : multiple types allowed, optional
+        Coordinate Reference System to use for the returned GeoDataFrame;
+        can accept anything accepted by pyproj.CRS.from_user_input()
+        
+            The following types are accepted:
+            CRS WKT string
+            An authority string (i.e. "epsg:4326")
+            An EPSG integer code (i.e. 4326)
+            A pyproj.CRS
+            An object with a to_wkt method.
+            PROJ string
+            Dictionary of PROJ parameters
+            PROJ keyword arguments for parameters
+            JSON string with PROJ parameters
+            
+            For reference, a few very common projections and their EPSG codes:
+            WGS84 Latitude/Longitude: "EPSG:4326"
+            UTM Zones (North): "EPSG:32633"
+            UTM Zones (South): "EPSG:32733"
+            
+            See geopandas/doc/source/projections.rst for more info on CRS
 
     See the documentation for pandas.read_sql for further explanation
     of the following parameters:
