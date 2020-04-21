6357bf3ff70e50acb7efed50a1b8411faa933649
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2020-04-21 09:42:13 +0200

    try the conda-forge way

---
+++ ci/travis/start_postgres.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash -e
+
+echo "Setting up Postgresql"
+
+mkdir -p ${PREFIX}/var
+rm -rf ${PREFIX}/var/db
+
+pg_ctl initdb -D ${PREFIX}/var/db
+pg_ctl start -D ${PREFIX}/var/db
+trap "stop_db; exit 0" HUP TERM TSTP
+trap "stop_db; exit 130" INT
+
+echo -n 'waiting for postgres'
+while [ ! -e /tmp/.s.PGSQL.5432 ]; do
+    sleep 1
+    echo -n '.'
+done
