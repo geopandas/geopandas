71c595063c46088a6858effcb2b057884385158b
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2020-04-21 11:53:28 +0200

    test

--- ci/travis/start_postgres.sh
+++ ci/travis/start_postgres.sh
@@ -6,7 +6,7 @@ mkdir -p ${HOME}/var
 rm -rf ${HOME}/var/db
 
 pg_ctl initdb -D ${HOME}/var/db
-createuser -s postgres
+createuser -U travis postgres
 pg_ctl start -D ${HOME}/var/db
 trap "stop_db; exit 0" HUP TERM TSTP
 trap "stop_db; exit 130" INT
