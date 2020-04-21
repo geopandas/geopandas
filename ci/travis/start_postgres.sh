656d5c6c166bfa1517fa812461e91c31b0c631a0
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2020-04-21 12:48:38 +0200

    cleanup

--- ci/travis/start_postgres.sh
+++ ci/travis/start_postgres.sh
@@ -7,8 +7,6 @@ rm -rf ${HOME}/var/db
 
 pg_ctl initdb -D ${HOME}/var/db
 pg_ctl start -D ${HOME}/var/db
-trap "stop_db; exit 0" HUP TERM TSTP
-trap "stop_db; exit 130" INT
 
 echo -n 'waiting for postgres'
 while [ ! -e /tmp/.s.PGSQL.5432 ]; do
