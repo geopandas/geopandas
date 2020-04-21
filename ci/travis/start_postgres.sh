bdac38614e03c35f93f45f7c1a4e8e8dd073c2d3
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2020-04-21 11:45:10 +0200

    test

--- ci/travis/start_postgres.sh
+++ ci/travis/start_postgres.sh
@@ -6,6 +6,7 @@ mkdir -p ${HOME}/var
 rm -rf ${HOME}/var/db
 
 pg_ctl initdb -D ${HOME}/var/db
+createuser -s postgres
 pg_ctl start -D ${HOME}/var/db
 trap "stop_db; exit 0" HUP TERM TSTP
 trap "stop_db; exit 130" INT
@@ -18,8 +19,10 @@ done
 
 psql --help
 
-createuser -s postgres
+
 
 createdb --owner=postgres geopandas_test
+
+psql -d geopandas_test -q -c "CREATE EXTENSION postgis"
 
 echo "Done setting up Postgresql"
