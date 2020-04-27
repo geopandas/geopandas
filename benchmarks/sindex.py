920b39ee5db331b0ea1bab53f5c0bbd6c21f1091
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2020-04-27 15:04:50 -0700

    add benchmarks

--- benchmarks/sindex.py
+++ benchmarks/sindex.py
@@ -1,6 +1,7 @@
 import random
 
 from geopandas import GeoDataFrame, GeoSeries
+from geopandas.sindex import VALID_QUERY_PREDICATES
 from shapely.geometry import Point, Polygon
 import numpy as np
 
@@ -49,3 +50,44 @@ class Bench:
     def time_sindex_intersects_objects(self, *args):
         for bounds in self.bounds:
             self.df.sindex.intersection(bounds, objects=True)
+
+
+class BenchQuery:
+
+    param_names = ["predicate"]
+    params = [*VALID_QUERY_PREDICATES]
+
+    def setup(self, *args):
+        triangles = GeoSeries(
+            [
+                Polygon([(random.random(), random.random()) for _ in range(3)])
+                for _ in range(100)
+            ]
+        )
+
+        points = GeoSeries(
+            [
+                Point(x, y)
+                for x, y in zip(
+                    np.random.random_sample(1000), np.random.random_sample(1000)
+                )
+            ]
+        )
+
+        df1 = GeoDataFrame(
+            {"val1": np.random.randn(len(triangles)), "geometry": triangles}
+        )
+        df2 = GeoDataFrame({"val1": np.random.randn(len(points)), "geometry": points})
+
+        self.df, self.df2, self.data = df1, df2, df2.geometry.values.data
+        self.bounds = [point.bounds for point in points]
+
+    def time_bulk_query_data(self, predicate):
+        self.df.sindex.query_bulk(self.data, predicate=predicate)
+
+    def time_bulk_query(self, predicate):
+        self.df.sindex.query_bulk(self.df2.geometry, predicate=predicate)
+
+    def time_query(self, predicate):
+        for geo in self.data:
+            self.df.sindex.query(geo, predicate=predicate)
