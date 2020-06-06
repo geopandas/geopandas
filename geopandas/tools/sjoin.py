8f758549ad2ffc1e0a5e8b2912f5b633820c1b4d
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2020-06-06 18:01:38 -0500

    remove import

--- geopandas/tools/sjoin.py
+++ geopandas/tools/sjoin.py
@@ -1,7 +1,6 @@
 import pandas as pd
 
 from geopandas import GeoDataFrame
-from geopandas._compat import HAS_RTREE
 from geopandas.array import _check_crs, _crs_mismatch_warn
 
 
@@ -64,13 +63,6 @@ def sjoin(
         raise ValueError(
             "'{0}' and '{1}' cannot be names in the frames being"
             " joined".format(index_left, index_right)
-        )
-
-    # Check if rtree is installed...
-    if not HAS_RTREE:
-        raise ImportError(
-            "Rtree must be installed to use sjoin\n\n"
-            "See installation instructions at https://geopandas.org/install.html"
         )
 
     # query index
