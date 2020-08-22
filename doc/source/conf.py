da3ae7f46954b270c0dbba5f69178e64b2993abc
Martin Fleischmann <martin@martinfleischmann.net>
2020-08-22 14:23:53 +0100

    favicon

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -171,7 +171,7 @@ html_logo = "_static/logo/geopandas_logo_web.svg"
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-# html_favicon = None
+html_favicon = "_static/logo/favicon.png"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
