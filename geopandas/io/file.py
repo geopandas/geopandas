9a0ec34c17e2e624109a2a7de0ff839ce6e699ee
Martin Fleischmann <martin@martinfleischmann.net>
2020-08-22 14:02:25 +0100

    POC: pyshp IO

--- geopandas/io/file.py
+++ geopandas/io/file.py
@@ -324,3 +324,121 @@ def _geometry_types(df):
         geom_types = geom_types[0]
 
     return geom_types
+
+
+def _read_pyshp(filename, **kwargs):
+    """Shapefile reader using pyshp instead of fiona"""
+    import shapefile
+
+    try:
+        shp = shapefile.Reader(filename, **kwargs)
+        records = shp.shapeRecords()
+    except UnicodeDecodeError:
+        import chardet
+
+        # guess encoding
+        with open(filename[:-3] + "dbf", "rb") as dbf:
+            encoding = chardet.detect(dbf.read())["encoding"]
+        shp = shapefile.Reader(filename, encoding=encoding)
+        records = shp.shapeRecords()
+
+    # fetch CRS
+    try:
+        with open(filename[:-3] + "prj", "r") as crs_wkt:
+            crs = pyproj.CRS.from_wkt(crs_wkt.read())
+    except FileNotFoundError:
+        crs = None
+
+    shp.close()
+    return GeoDataFrame.from_features(records.__geo_interface__, crs=crs)
+
+
+def _write_pyshp(df, filename, index=False):
+    """
+    Shapefile writer using pyshp instead of fiona
+
+    Missing:
+    - specification of field size (precision, lenght)
+    """
+    import shapefile
+
+    if any([len(c) > 10 for c in df.columns.tolist()]):
+        warnings.warn(
+            "Column names longer than 10 characters will be truncated when saved to "
+            "ESRI Shapefile.",
+            stacklevel=2,
+        )
+
+    fields_mapping = {
+        "i": "N",
+        "u": "N",
+        "f": "N",
+        "c": "N",
+        "M": "D",
+        "O": "C",
+        "b": "L",
+        "S": "C",
+        "U": "C",
+    }
+
+    geometry = df.geometry
+    data = df.drop(columns="geometry")
+    if index:
+        data = data.reset_index()
+
+    with shapefile.Writer(filename) as w:
+        for name, dtype in data.dtypes.iteritems():
+            w.field(str(name), fields_mapping[dtype.kind])
+
+        for t in data.itertuples(index=False):
+            w.record(*t)
+
+        for geom in geometry:
+            if geom.type == "Point":
+                if geom.has_z:
+                    w.pointz(geom.x, geom.y, geom.z)
+                else:
+                    w.point(geom.x, geom.y)
+            elif geom.type == "MultiPoint":
+                if geom.has_z:
+                    w.multipointz(np.asarray(geom))
+                else:
+                    w.multipoint(np.asarray(geom))
+            elif geom.type == "LineString":
+                if geom.has_z:
+                    w.linez([list(geom.coords)])
+                else:
+                    w.line([list(geom.coords)])
+            elif geom.type == "MultiLineString":
+                if geom.has_z:
+                    w.linez([list(g.coords) for g in geom.geoms])
+                else:
+                    w.line([list(g.coords) for g in geom.geoms])
+            elif geom.type == "Polygon":
+                c = []
+                c.append(list(geom.exterior.coords))
+                for i in geom.interiors:
+                    c.append(list(i.coords))
+                if geom.has_z:
+                    w.polyz(c)
+                else:
+                    w.poly(c)
+            elif geom.type == "MultiPolygon":
+                c = []
+                for g in geom.geoms:
+                    c.append(list(g.exterior.coords))
+                    for i in g.interiors:
+                        c.append(list(i.coords))
+                if geom.has_z:
+                    w.polyz(c)
+                else:
+                    w.poly(c)
+            elif geom.type == "GeometryCollection":
+                raise ValueError(
+                    "Geometry type of 'Geometry Collection' is not supported in SHP."
+                )
+
+    # write CRS to .prj
+    if df.crs:
+        with open(filename[:-3] + "prj", "w") as prj:
+            prj.write(df.crs.to_wkt(version="WKT1_ESRI"))
