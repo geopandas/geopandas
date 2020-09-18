3eec96daaaf768c1c5b2862742661432cc8f680b
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2020-09-18 16:58:49 -0500

    speed up emptiness and size checks for rtree

--- geopandas/sindex.py
+++ geopandas/sindex.py
@@ -252,11 +252,16 @@ if compat.HAS_RTREE:
 
         @property
         def size(self):
-            return len(self.leaves()[0][1])
+            if hasattr(self, "_size"):
+                size = self._size
+            else:
+                size = len(self.leaves()[0][1])
+                self._size = size
+            return size
 
         @property
         def is_empty(self):
-            return self.size == 0
+            return self.geometries.size == 0 or self.size == 0
 
         def __len__(self):
             return self.size
