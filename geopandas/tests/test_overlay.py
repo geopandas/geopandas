4a650af20223ffd376c7f0f4307d31a243cbb98e
James McBride <jdmcbr@gmail.com>
2021-02-18 12:36:56 -0800

    ENH: Add warning if keep_geom_type drops geometries during overlay (#1554)
    * Add warning if keep_geom_type drops geometries during overlay
    
    * Expand keep_geom_type warning message
    
    * Add test for keep_geom_type dropping geoms
    
    * Set keep_geom_type=None by default
    
    With True, no warning raised. With None, will drop geoms of other types,
    but warn the user.
    
    * Update geopandas/tests/test_overlay.py
    
    Co-authored-by: Martin Fleischmann <martin@martinfleischmann.net>
    
    Co-authored-by: Joris Van den Bossche <jorisvandenbossche@gmail.com>
    Co-authored-by: Martin Fleischmann <martin@martinfleischmann.net>

--- geopandas/tests/test_overlay.py
+++ geopandas/tests/test_overlay.py
@@ -382,8 +382,24 @@ def test_correct_index(dfs):
     expected = GeoDataFrame(
         [[1, 1, i1], [3, 2, i2]], columns=["col3", "col2", "geometry"]
     )
-    result = overlay(df3, df2)
+    result = overlay(df3, df2, keep_geom_type=True)
     assert_geodataframe_equal(result, expected)
+
+
+def test_warn_on_keep_geom_type(dfs):
+
+    df1, df2 = dfs
+    polys3 = GeoSeries(
+        [
+            Polygon([(1, 1), (3, 1), (3, 3), (1, 3)]),
+            Polygon([(-1, 1), (1, 1), (1, 3), (-1, 3)]),
+            Polygon([(3, 3), (5, 3), (5, 5), (3, 5)]),
+        ]
+    )
+    df3 = GeoDataFrame({"geometry": polys3})
+
+    with pytest.warns(UserWarning, match="`keep_geom_type=True` in overlay"):
+        overlay(df2, df3, keep_geom_type=None)
 
 
 @pytest.mark.parametrize(
