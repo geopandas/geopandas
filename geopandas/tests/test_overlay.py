f1d0c9e05e74db3be921d6fbf860cb74d1e890a6
unknown <abhinav04111998@gmail.com>
2021-04-07 23:09:09 +0530

    updated

--- geopandas/tests/test_overlay.py
+++ geopandas/tests/test_overlay.py
@@ -12,9 +12,6 @@ from geopandas.testing import assert_geodataframe_equal, assert_geoseries_equal
 import pytest
 
 
-
-
-
 DATA = os.path.join(os.path.abspath(os.path.dirname(__file__)), "data", "overlay")
 
 
@@ -600,36 +597,84 @@ def test_overlap_make_valid(make_valid):
 
 class TestOverlayWikiExample:
     def setup_method(self):
-        self.layer_a = GeoDataFrame([{'geometry': box(0, 2, 6, 6)}])
+        self.layer_a = GeoDataFrame([{"geometry": box(0, 2, 6, 6)}])
 
-        self.layer_b = GeoDataFrame([{'geometry': box(4, 0, 10, 4)}])
+        self.layer_b = GeoDataFrame([{"geometry": box(4, 0, 10, 4)}])
 
-        self.intersection = GeoDataFrame([{'geometry': box(4, 2, 6, 4)}])
+        self.intersection = GeoDataFrame([{"geometry": box(4, 2, 6, 4)}])
 
         self.union = GeoDataFrame(
-            [{'geometry': box(4, 2, 6, 4)},
-             {'geometry':Polygon([(4,2), (0,2), (0,6), (6,6), (6,4), (4,4), (4,2)])},
-             {'geometry': Polygon([(10,0),(4,0),(4,2),(6,2),(6,4),(10,4),(10,0)])}])
+            [
+                {"geometry": box(4, 2, 6, 4)},
+                {
+                    "geometry": Polygon(
+                        [(4, 2), (0, 2), (0, 6), (6, 6), (6, 4), (4, 4), (4, 2)]
+                    )
+                },
+                {
+                    "geometry": Polygon(
+                        [(10, 0), (4, 0), (4, 2), (6, 2), (6, 4), (10, 4), (10, 0)]
+                    )
+                },
+            ]
+        )
 
         self.a_difference_b = GeoDataFrame(
-            [{'geometry':Polygon([(4,2), (0,2), (0,6), (6,6), (6,4), (4,4), (4,2)])}])
+            [
+                {
+                    "geometry": Polygon(
+                        [(4, 2), (0, 2), (0, 6), (6, 6), (6, 4), (4, 4), (4, 2)]
+                    )
+                }
+            ]
+        )
 
         self.b_difference_a = GeoDataFrame(
-            [{'geometry': Polygon([(10,0),(4,0),(4,2),(6,2),(6,4),(10,4),(10,0)])}])
-
+            [
+                {
+                    "geometry": Polygon(
+                        [(10, 0), (4, 0), (4, 2), (6, 2), (6, 4), (10, 4), (10, 0)]
+                    )
+                }
+            ]
+        )
 
         self.symmetric_difference = GeoDataFrame(
-            [{'geometry':Polygon([(4,2), (0,2), (0,6), (6,6), (6,4), (4,4), (4,2)])},
-             {'geometry': Polygon([(10,0), (4,0), (4,2), (6,2), (6,4), (10,4), (10,0)])}])
+            [
+                {
+                    "geometry": Polygon(
+                        [(4, 2), (0, 2), (0, 6), (6, 6), (6, 4), (4, 4), (4, 2)]
+                    )
+                },
+                {
+                    "geometry": Polygon(
+                        [(10, 0), (4, 0), (4, 2), (6, 2), (6, 4), (10, 4), (10, 0)]
+                    )
+                },
+            ]
+        )
 
-        self.a_identity_b = GeoDataFrame([
-            {'geometry': box(4, 2, 6, 4)},
-            {'geometry':Polygon([(4,2), (0,2), (0,6), (6,6), (6,4), (4,4), (4,2)])}])
+        self.a_identity_b = GeoDataFrame(
+            [
+                {"geometry": box(4, 2, 6, 4)},
+                {
+                    "geometry": Polygon(
+                        [(4, 2), (0, 2), (0, 6), (6, 6), (6, 4), (4, 4), (4, 2)]
+                    )
+                },
+            ]
+        )
 
-        self.b_identity_a = GeoDataFrame([
-            {'geometry': box(4, 2, 6, 4)},
-            {'geometry': Polygon([(10,0),(4,0),(4,2),(6,2),(6,4),(10,4),(10,0)])}])
-
+        self.b_identity_a = GeoDataFrame(
+            [
+                {"geometry": box(4, 2, 6, 4)},
+                {
+                    "geometry": Polygon(
+                        [(10, 0), (4, 0), (4, 2), (6, 2), (6, 4), (10, 4), (10, 0)]
+                    )
+                },
+            ]
+        )
 
     def test_intersection(self):
         df_result = overlay(self.layer_a, self.layer_b, how="intersection")
@@ -638,6 +683,7 @@ class TestOverlayWikiExample:
     def test_union(self):
         df_result = overlay(self.layer_a, self.layer_b, how="union")
         assert_geodataframe_equal(df_result, self.union)
+
     def test_a_difference_b(self):
         df_result = overlay(self.layer_a, self.layer_b, how="difference")
         assert_geodataframe_equal(df_result, self.a_difference_b)
@@ -647,8 +693,7 @@ class TestOverlayWikiExample:
         assert_geodataframe_equal(df_result, self.b_difference_a)
 
     def test_symmetric_difference(self):
-        df_result = overlay(self.layer_a, self.layer_b,
-                            how="symmetric_difference")
+        df_result = overlay(self.layer_a, self.layer_b, how="symmetric_difference")
         assert_geodataframe_equal(df_result, self.symmetric_difference)
 
     def test_a_identity_b(self):
