028d8afbceb39093e4dd3de49c0616cac6cfeb77
Zero <Zeroto521@gmail.com>
2021-05-30 19:00:07 +0800

    Add annotations and blank line

--- geopandas/accessor.py
+++ geopandas/accessor.py
@@ -3,12 +3,14 @@ from pandas.util._decorators import doc
 
 
 @doc(_register_accessor, klass="GeoDataFrame")
-def register_geodataframe_accessor(name):
+def register_geodataframe_accessor(name: str):
     from geopandas import GeoDataFrame
+
     return _register_accessor(name, GeoDataFrame)
 
 
 @doc(_register_accessor, klass="GeoSeries")
-def register_geoseries_accessor(name):
+def register_geoseries_accessor(name: str):
     from geopandas import GeoSeries
+
     return _register_accessor(name, GeoSeries)
