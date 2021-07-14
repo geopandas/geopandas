73e4ba4321ddf591c1e00866799ebf03f518d3b0
Steve Berdy <steveberdy@gmail.com>
2021-07-14 11:30:08 -0400

    Remove last optional geopy type hint

--- geopandas/tools/geocoding.py
+++ geopandas/tools/geocoding.py
@@ -8,7 +8,7 @@ from shapely.geometry import Point
 import geopandas
 
 
-def _get_throttle_time(provider: geopy.geocoders.base.Geocoder) -> int:
+def _get_throttle_time(provider) -> int:
     """
     Amount of time to wait between requests to a geocoding API, for providers
     that specify rate limits in their terms of service.
