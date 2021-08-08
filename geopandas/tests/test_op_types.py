eb424e8fe26c511e8d25b7c63560ff5135f8f234
Matt Richards <matt.richards@veitchlister.com.au>
2021-08-08 12:25:42 +1000

    TST: more tests

--- geopandas/tests/test_op_types.py
+++ geopandas/tests/test_op_types.py
@@ -7,7 +7,7 @@ import pandas as pd
 from shapely.geometry import Point
 
 import geopandas
-from geopandas import GeoDataFrame, read_file
+from geopandas import GeoDataFrame, read_file, GeoSeries
 
 from geopandas.tests.util import PACKAGE_DIR
 
@@ -22,7 +22,12 @@ class TestDataFrame:
         self.crs = "epsg:4326"
         self.df2 = GeoDataFrame(
             [
-                {"geometry": Point(x, y), "value1": x + y, "value2": x * y}
+                {
+                    "geometry": Point(x, y),
+                    "value1": x + y,
+                    "value2": x * y,
+                    "geometry2": Point(x, y),
+                }
                 for x, y in zip(range(N), range(N))
             ],
             crs=self.crs,
@@ -38,20 +43,40 @@ class TestDataFrame:
         assert type(self.df2) is GeoDataFrame
         assert self.df2.crs == self.crs
 
-    def test_drop(self):
-        assert type(self.df.drop(columns="geometry")) is pd.DataFrame
-
     def test_getitem(self):
         assert type(self.df2[["value1", "value2"]]) is pd.DataFrame
+        assert type(self.df2["geometry"]) is GeoSeries
+        assert type(self.df2["geometry2"]) is GeoSeries
+        assert type(self.df2["value1"]) is pd.Series
+        assert type(self.df2[["geometry"]]) is GeoDataFrame
+        assert type(self.df2[["value1"]]) is pd.DataFrame
+        assert type(self.df2[["geometry2"]]) is GeoDataFrame
 
     def test_loc(self):
         assert type(self.df2.loc[:, ["value1", "value2"]]) is pd.DataFrame
+        assert type(self.df2.loc[:, "geometry"]) is GeoSeries
+        assert type(self.df2.loc[:, "geometry2"]) is GeoSeries
+        assert type(self.df2["value1"]) is pd.Series
+        assert type(self.df2.loc[:, ["geometry"]]) is GeoDataFrame
+        assert type(self.df2.loc[:, ["value1"]]) is pd.DataFrame
+        assert type(self.df2.loc[:, ["geometry2"]]) is GeoDataFrame
+
+    def test_squeeze(self):
+        assert type(self.df2[["geometry"]].squeeze()) is GeoSeries
+        assert type(self.df2[["geometry2"]].squeeze()) is GeoSeries
+
+    def test_to_frame(self):
+        assert type(self.df2["geometry"].to_frame()) is GeoDataFrame
+        assert type(self.df2["geometry2"].to_frame()) is GeoDataFrame
 
     def test_iloc(self):
-        df = self.df2.iloc[:, 1:]
-        print(df.columns)
-
-        assert type(df) is pd.DataFrame
+        assert type(self.df2.iloc[:, 1:3]) is pd.DataFrame
+        assert type(self.df2.iloc[:, 0]) is GeoSeries
+        assert type(self.df2.iloc[:, 3]) is GeoSeries
+        assert type(self.df2.iloc[:, 1]) is pd.Series
+        assert type(self.df2.iloc[:, [0]]) is GeoDataFrame
+        assert type(self.df2.iloc[:, [1]]) is pd.DataFrame
+        assert type(self.df2.iloc[:, [3]]) is GeoDataFrame
 
     def test_reindex(self):
         df = geopandas.read_file(geopandas.datasets.get_path("naturalearth_cities"))
@@ -60,3 +85,14 @@ class TestDataFrame:
         print(df)
         print(df_reindex)
         assert type(df_reindex) is pd.DataFrame
+
+    def test_drop(self):
+        assert type(self.df.drop(columns="geometry")) is pd.DataFrame
+
+    def test_apply(self):
+        assert type(self.df2["geometry"].apply(lambda x: x)) is GeoSeries
+        assert type(self.df2["geometry2"].apply(lambda x: x)) is GeoSeries
+        assert type(self.df2["value1"].apply(lambda x: x)) is pd.Series
+
+        assert type(self.df2.apply(lambda x: x)) is GeoDataFrame
+        assert type(self.df2[["value1", "value2"]].apply(lambda x: x)) is pd.DataFrame
