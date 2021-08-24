b9e340d3c4be86a659be6fa7f6eba2804e2fc5fa
Matt Richards <matt.richards@veitchlister.com.au>
2021-08-24 22:17:50 +1000

    TST: add test for missing case identified by codecov

--- geopandas/tests/test_merge.py
+++ geopandas/tests/test_merge.py
@@ -97,3 +97,18 @@ class TestMerging:
         res3 = pd.concat([df2.set_crs("epsg:4326"), self.gdf], axis=1)
         # check metadata comes from first df
         self._check_metadata(res3, geometry_column_name="geom", crs="epsg:4326")
+
+    def test_concat_axis1_geoseries(self):
+        gseries2 = GeoSeries([Point(i, i) for i in range(3, 6)], crs="epsg:4326")
+        result = pd.concat([gseries2, self.gseries], axis=1)
+        # result columns are [0, 1], not an explicit way to rename them.
+        # leave geometry column as None and user to sort out.
+        # This could happen in a less contrived way via DataFrame.convert_dtypes
+        assert type(result) is GeoDataFrame
+        assert result._geometry_column_name is None
+
+        gseries2.name = "foo"
+        result2 = pd.concat([gseries2, self.gseries], axis=1)
+        print(result2)
+        assert type(result2) is GeoDataFrame
+        self._check_metadata(result2, geometry_column_name="foo", crs="epsg:4326")
