b67ba848a7afa344d335c4ce78bbe310e809e1fe
Matt Richards <45483497+m-richards@users.noreply.github.com>
2021-09-20 18:45:38 +1000

    BUG: convert_dtypes() converts GeoDataFrame to DataFrame (#2115)
    * TST: test documenting existing convert dtypes behaviour
    
    * TST: fix tests, compare to proper reference case
    
    * BUG: overload convert_dtypes to fix GH1870
    
    * COMPAT: update tests to handle pandas 1.0 and 0.25
    
    * DOC: update wording to suggested
    
    Co-authored-by: Martin Fleischmann <martin@martinfleischmann.net>
    
    * CLN: simplify to *args and **kwargs following suggestion
    
    Co-authored-by: Martin Fleischmann <martin@martinfleischmann.net>

--- geopandas/geodataframe.py
+++ geopandas/geodataframe.py
@@ -1652,6 +1652,34 @@ box': (2.0, 1.0, 2.0, 1.0)}], 'bbox': (1.0, 1.0, 2.0, 2.0)}
         # do not return a GeoDataFrame
         return pd.DataFrame(df)
 
+    def convert_dtypes(self, *args, **kwargs):
+        """
+        Convert columns to best possible dtypes using dtypes supporting ``pd.NA``.
+
+        Always returns a GeoDataFrame as no conversions are applied to the
+        geometry column.
+
+        See the pandas.DataFrame.convert_dtypes docstring for more details.
+
+        Returns
+        -------
+        GeoDataFrame
+
+        """
+        # Overridden to fix GH1870, that return type is not preserved always
+        # (and where it was, geometry col was not)
+
+        if not compat.PANDAS_GE_10:
+            raise NotImplementedError(
+                "GeoDataFrame.convert_dtypes requires pandas >= 1.0"
+            )
+
+        return GeoDataFrame(
+            super().convert_dtypes(*args, **kwargs),
+            geometry=self.geometry.name,
+            crs=self.crs,
+        )
+
     def to_postgis(
         self,
         name,
