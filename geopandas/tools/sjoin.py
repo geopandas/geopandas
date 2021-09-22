e6da2afcf7cbe424b2bedf87a1761683581791bc
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2021-09-21 22:34:57 -0500

    remove duplicate loads

--- geopandas/tools/sjoin.py
+++ geopandas/tools/sjoin.py
@@ -452,9 +452,6 @@ Italy    ITA   2221000.0
     1      San Marino  POINT (12.44177 43.93610)          141  62137802    Europe      \
 Italy    ITA   2221000.0
     To include the distances:
-    >>> countries = geopandas.read_file(geopandas.datasets.get_\
-path("naturalearth_lowres"))
-    >>> cities = geopandas.read_file(geopandas.datasets.get_path("naturalearth_cities"))
     >>> cities_w_country_data = geopandas.sjoin_nearest\
 (cities, countries, distance_col="distances")
     >>> cities_w_country_data[["name_left", "name_right", \
