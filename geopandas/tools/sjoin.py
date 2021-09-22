2a0351f753ffb62723f0d96c672aa58faaf2d5ac
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2021-09-21 22:32:13 -0500

    Update geopandas/tools/sjoin.py
    Co-authored-by: Brendan Ward <bcward@astutespruce.com>

--- geopandas/tools/sjoin.py
+++ geopandas/tools/sjoin.py
@@ -464,7 +464,7 @@ path("naturalearth_lowres"))
     0    Vatican City      Italy       0.0
     1      San Marino      Italy       0.0
     In the following example we get multiple cities for Italy because all results are
-    equidistant (in this case zero). In fact, we get 3 results in total:
+    equidistant (in this case zero because they intersect). In fact, we get 3 results in total:
     >>> countries_w_city_data = geopandas.sjoin_nearest\
 (cities, countries, distance_col="distances", how="right")
     >>> italy_results = \
