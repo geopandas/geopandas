a6b1a12b272f6df4a4b526a1ba5455e80d1a07c3
Adrian Tofting <adrian.tofting@iterate.no>
2021-10-08 14:24:35 +0200

    Format with Black

--- geopandas/geoseries.py
+++ geopandas/geoseries.py
@@ -921,7 +921,9 @@ class GeoSeries(GeoPandasBase, Series):
         index = []
         geometries = []
         for idx, s in self.geometry.iteritems():
-            if s.type.startswith("Multi") or (s.type == "GeometryCollection" and not s.is_empty):
+            if s.type.startswith("Multi") or (
+                s.type == "GeometryCollection" and not s.is_empty
+            ):
                 geoms = s.geoms
                 idxs = [(idx, i) for i in range(len(geoms))]
             else:
