984e14dc6e74b03f897f347c726a4d04c60a965e
Ray Bell <rayjohnbell0@gmail.com>
2021-10-27 13:56:32 -0400

    Update geopandas/tools/sjoin.py
    Co-authored-by: Brendan Ward <bcward@astutespruce.com>

--- geopandas/tools/sjoin.py
+++ geopandas/tools/sjoin.py
@@ -417,7 +417,7 @@ def sjoin_nearest(
     Results will include multiple output records for a single input record
     where there are multiple equidistant nearest or intersected neighbors.
 
-    Distant is calculated in CRS units and can be returned using the
+    Distance is calculated in CRS units and can be returned using the
     `distance_col` parameter.
 
     See the User Guide page
