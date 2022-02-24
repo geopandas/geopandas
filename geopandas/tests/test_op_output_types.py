ae77faa92fb22a9df64711dce207e2a47caf72fe
Matt Richards <mrichards7@outlook.com.au>
2022-02-24 21:57:41 +1000

    COMPAT: old pandas

--- geopandas/tests/test_op_output_types.py
+++ geopandas/tests/test_op_output_types.py
@@ -283,4 +283,8 @@ def test_expanddim_in_unstack():
     )
     unstack = s.unstack()
     assert_object(unstack, GeoDataFrame, None, None, False)
-    assert unstack._geometry_column_name is None
+
+    if compat.PANDAS_GE_12:
+        assert unstack._geometry_column_name is None
+    else:  # pandas GH37369, unstack doesn't call finalize
+        assert unstack._geometry_column_name == "geometry"
