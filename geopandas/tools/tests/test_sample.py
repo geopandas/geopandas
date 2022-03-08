268d2885820f21141dede1a8b69e52c1efc6db25
ljwolf <levi.john.wolf@gmail.com>
2022-03-08 15:20:07 +0000

    revise sampling frame to remove demo scripts

--- geopandas/tools/tests/test_sample.py
+++ geopandas/tools/tests/test_sample.py
@@ -1,99 +1,76 @@
+from sklearn import random_projection
 import geopandas
 
+import pytest
+from shapely import geometry
+from scipy.spatial import distance
+from geopandas.tools._random import uniform, grid
 
-# types
 multipolygons = geopandas.read_file(geopandas.datasets.get_path("nybb")).geometry
 polygons = multipolygons.explode().geometry
 multilinestrings = multipolygons.boundary
 linestrings = polygons.boundary
-multipoints = None
 points = multipolygons.centroid
-mixed = geopandas.pd.concat(
-    (multipolygons.geometry.head(2), multilinestrings.geometry.tail(2), points)
+
+
+multipolygon = multipolygons.geometry.iloc[0]
+polygon = polygons.geometry.iloc[0]
+multilinestring = multilinestrings.geometry.iloc[0]
+linestring = linestrings.geometry.iloc[0]
+point = points.geometry.iloc[0]
+
+
+@pytest.mark.parametrize(
+    ["data", "size", "batch_size"],
+    [
+        (data, size, batch_size)
+        for data in (multipolygon, polygon)
+        for size in (None, 10)
+        for batch_size in (None, 2)
+    ],
 )
+def test_uniform(data, size, batch_size):
+    sample = uniform(data, size=size, batch_size=batch_size)
+
+    assert (
+        len(sample) == size
+    ), f"{size} points were requested, but {len(sample)} were drawn."
+    sample_in_geom = (
+        geopandas.GeoSeries(sample).explode().sindex.query(data, predicate="intersects")
+    )
+    assert len(sample_in_geom) == size
 
 
-multipolygon = multipolygons.geometry.values.data[0]
-polygon = polygons.geometry.values.data[0]
-multilinestring = multilinestrings.geometry.values.data[0]
-linestring = linestrings.geometry.values.data[0]
+@pytest.mark.parametrize(
+    ["data", "size", "spacing", "method"],
+    [
+        (data, size, spacing, method)
+        for data in (multipolygon, polygon, multilinestring, linestring)
+        for size in (None, 10, (5, 3))
+        for spacing in (None, 500)
+        for method in ("hex", "square")
+        if ((not ((spacing is not None) and (size is not None))))
+    ],
+)
+def test_grid(data, size, spacing, method):
 
-
-from geopandas.tools.grids import make_grid
-from geopandas.tools._random import uniform
-from matplotlib import pyplot as plt
-
-make_grid(polygons)
-
-f, ax = plt.subplots(6, 4, figsize=(10, 15))
-
-for ji, method in enumerate(("random", "grid")):
-    for jj, tile in enumerate((None, "square", "hex")):
-        if method == "grid":
-            size = (10, 10)
+    sample = grid(data, size=size, spacing=spacing, method=method)
+    if spacing is not None:
+        a, b, *rest = sample.geoms
+        assert a.distance(b) == spacing
+    else:
+        if isinstance(size, tuple):
+            n_specified = size[0] * size[1]
+        elif size is None:
+            n_specified = 100
         else:
-            size = 10
-        overall = multipolygons.geometry.sample_points(
-            size=size, method=method, tile=tile
-        )
-        assert isinstance(overall, geopandas.GeoSeries)
+            n_specified = size
+        assert (
+            len(sample.geoms) <= n_specified
+        ), f"Sampled {len(sample.geoms)} points instead of {n_specified} for {data.type}"
 
-        line_overall = multilinestrings.geometry.sample_points(
-            size=size, method=method, tile=tile
-        )
-        assert isinstance(line_overall, geopandas.GeoSeries)
-
-        point_overall = points.geometry.sample_points(
-            size=size, method=method, tile=tile
-        )
-        assert isinstance(point_overall, geopandas.GeoSeries)
-
-        mixed_overall = mixed.geometry.sample_points(
-            size=size, method=method, tile=tile
-        )
-        assert isinstance(mixed_overall, geopandas.GeoSeries)
-
-        j = ji * 3 + jj
-        for i in range(4):
-            sample, data = (
-                (overall, multipolygons),
-                (line_overall, multilinestrings),
-                (point_overall, points),
-                (mixed_overall, mixed),
-            )[i]
-            data.plot(ax=ax[j, i], zorder=-1)
-            sample.plot(ax=ax[j, i], color="r", marker=".", markersize=10)
-
-            if i == 0:
-                ax[j, i].set_ylabel(f"{method}\n{tile}")
-            if j == 0:
-                ax[j, i].set_title(("polygons", "lines", "points", "mixed")[i])
-            ax[j, i].set_xticks([])
-            ax[j, i].set_xticklabels([])
-            ax[j, i].set_yticks([])
-            ax[j, i].set_yticklabels([])
-
-f.tight_layout()
-plt.show()
+    # check all points intersect the input geom
 
 
-# f, ax = plt.subplots(2, 4, figsize=(16, 4))
-# for i in range(8):
-#    ax_ = ax.flatten()[i]
-#
-#    df = (
-#        overall,
-#        overall_multi,
-#        line_overall,
-#        line_overall_multi,
-#        point_overall,
-#        point_overall_multi,
-#        mixed_overall,
-#        mixed_overall_multi,
-#    )[i]
-#    df.set_crs(polygons.crs)
-#    target = multipolygons if i < 4 else multilinestrings
-#    target.plot(ax=ax_)
-#    df.plot(ax=ax_, color="k")
-# f.tight_layout()
-# plt.show()
+def test_size_and_spacing_failures():
+    ...
