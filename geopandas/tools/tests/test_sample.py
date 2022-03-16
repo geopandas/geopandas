fb490598c819f55411b07543d153643ae7b367e4
ljwolf <levi.john.wolf@gmail.com>
2022-03-16 16:35:22 +0000

    finalize testing

--- geopandas/tools/tests/test_sample.py
+++ geopandas/tools/tests/test_sample.py
@@ -1,10 +1,11 @@
-from sklearn import random_projection
 import geopandas
 
 import pytest
 from shapely import geometry
 from scipy.spatial import distance
+import numpy
 from geopandas.tools._random import uniform, grid
+import warnings
 
 multipolygons = geopandas.read_file(geopandas.datasets.get_path("nybb")).geometry
 polygons = multipolygons.explode().geometry
@@ -14,10 +15,23 @@ points = multipolygons.centroid
 
 
 multipolygon = multipolygons.geometry.iloc[0]
-polygon = polygons.geometry.iloc[0]
+polygon = polygons.geometry.iloc[2]
 multilinestring = multilinestrings.geometry.iloc[0]
 linestring = linestrings.geometry.iloc[0]
 point = points.geometry.iloc[0]
+
+
+def find_spacing(input, return_distances=False):
+    if not (isinstance(input, (geopandas.GeoSeries, geopandas.GeoDataFrame))):
+        input = geopandas.GeoSeries([g for g in input.geoms])
+    x, y = input.x, input.y
+    first = numpy.array([[x[0], y[0]]])
+    rest = numpy.column_stack((x[1:], y[1:]))
+    distances = distance.cdist(first, rest)
+    if return_distances:
+        return distances.min(), distances
+    else:
+        return distances.min()
 
 
 @pytest.mark.parametrize(
@@ -30,47 +44,64 @@ point = points.geometry.iloc[0]
     ],
 )
 def test_uniform(data, size, batch_size):
-    sample = uniform(data, size=size, batch_size=batch_size)
-
+    if size is None:
+        sample = uniform(data, batch_size=batch_size)
+    else:
+        sample = uniform(data, batch_size=batch_size, size=size)
+    size = 1 if size is None else size
+    sample_series = geopandas.GeoSeries(sample).explode().reset_index(drop=True)
     assert (
-        len(sample) == size
-    ), f"{size} points were requested, but {len(sample)} were drawn."
-    sample_in_geom = (
-        geopandas.GeoSeries(sample).explode().sindex.query(data, predicate="intersects")
-    )
+        len(sample_series) == size
+    ), f"{size} points were requested, but {len(sample_series)} were drawn."
+    sample_in_geom = sample_series.sindex.query(data, predicate="intersects")
     assert len(sample_in_geom) == size
 
 
 @pytest.mark.parametrize(
-    ["data", "size", "spacing", "method"],
+    ["data", "size", "spacing", "method", "seed"],
     [
-        (data, size, spacing, method)
+        (data, size, spacing, method, seed)
         for data in (multipolygon, polygon, multilinestring, linestring)
         for size in (None, 10, (5, 3))
         for spacing in (None, 500)
         for method in ("hex", "square")
+        for seed in (112112, 123456)
         if ((not ((spacing is not None) and (size is not None))))
     ],
 )
-def test_grid(data, size, spacing, method):
-
+def test_grid(data, size, spacing, method, seed):
+    numpy.random.seed(seed)
     sample = grid(data, size=size, spacing=spacing, method=method)
     if spacing is not None:
-        a, b, *rest = sample.geoms
-        assert a.distance(b) == spacing
+        implied_spacing = find_spacing(sample)
+        assert (
+            implied_spacing <= spacing
+        ), f"spacing is wrong for size={size}, spacing={spacing}, method={method} on {data.type}"
     else:
         if isinstance(size, tuple):
             n_specified = size[0] * size[1]
         elif size is None:
             n_specified = 100
+        elif data.type in ("Polygon", "MultiPolygon"):
+            n_specified = size ** 2
         else:
             n_specified = size
-        assert (
-            len(sample.geoms) <= n_specified
-        ), f"Sampled {len(sample.geoms)} points instead of {n_specified} for {data.type}"
+        assert (len(sample.geoms) - n_specified) <= 1, (
+            f"Sampled {len(sample.geoms)} points instead of "
+            f"{n_specified} for size={size}, spacing={spacing}, "
+            f"method={method} on {data.type}"
+        )  # This is an artefact of the way lines are sampled; check numpy.arange docstring for the details.
 
-    # check all points intersect the input geom
+    points_in_grid = geopandas.GeoSeries(sample).explode()
+    intersects = points_in_grid.sindex.query(data.buffer(1e-7), predicate="intersects")
+    assert len(intersects) == len(points_in_grid)
 
 
 def test_size_and_spacing_failures():
-    ...
+    for bad_size in [(10, 10, 2), 0.1, (0.1, 0.2)]:
+        with pytest.raises(TypeError):
+            grid(size=bad_size)
+        with pytest.raises(TypeError):
+            uniform(size=bad_size)
+    with pytest.raises(ValueError):
+        grid(size=10, spacing=1)
