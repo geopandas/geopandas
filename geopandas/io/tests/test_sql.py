8181092db0adf44a9c4e6e079e6702c0c359ff7f
latot <felipematas@yahoo.com>
2022-03-28 11:24:09 -0300

    make it beautiful 5

--- geopandas/io/tests/test_sql.py
+++ geopandas/io/tests/test_sql.py
@@ -690,13 +690,14 @@ class TestIO:
         table = "nybb"
         sql = """
         SELECT c.column_name, c.data_type
-        FROM information_schema.table_constraints tc 
-        JOIN information_schema.constraint_column_usage AS ccu USING (constraint_schema, constraint_name) 
+        FROM information_schema.table_constraints tc
+        JOIN information_schema.constraint_column_usage
+        AS ccu USING (constraint_schema, constraint_name)
         JOIN information_schema.columns AS c ON c.table_schema = tc.constraint_schema
         AND tc.table_name = c.table_name AND ccu.column_name = c.column_name
         WHERE constraint_type = 'PRIMARY KEY' and tc.table_name = '{table}';
         """.format(
-            table
+            table=table
         )
         engine = engine_postgis
         # write table without primery key, table as read-only for QGIS
