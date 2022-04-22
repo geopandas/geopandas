78549dbbac9d890de1bb1ad026138761d45a55be
Mike Taves <mwtoews@gmail.com>
2022-04-23 00:38:49 +1200

    Address PR feedback

--- setup.py
+++ setup.py
@@ -8,6 +8,9 @@ import sys
 
 from setuptools import setup
 
+# ensure the current directory is on sys.path so versioneer can be imported
+# when pip uses PEP 517/518 build rules.
+# https://github.com/python-versioneer/python-versioneer/issues/193
 sys.path.append(os.path.dirname(__file__))
 
 import versioneer  # noqa: E402
