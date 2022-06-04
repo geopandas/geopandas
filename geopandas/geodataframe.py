3792927cf2a901d23284e6fed0dfe4dd78fe4c18
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2022-06-04 12:17:37 +0200

    add comment

--- geopandas/geodataframe.py
+++ geopandas/geodataframe.py
@@ -1468,7 +1468,21 @@ individually so that features may have different properties
     @property
     def _constructor_sliced(self):
         def _geodataframe_constructor_sliced(data=None, index=None, crs=None, **kwargs):
+            """
+            A specialized (Geo)Series constructor which can fall back to a
+            Series if a certain operation does not produce geometries:
 
+            - We only return a GeoSeries if the data is actually of geometry
+              dtype (and so we don't try to convert geometry objects such as
+              the normal GeoSeries(..) constructor does with `_ensure_geometry`).
+            - When we get here from obtaining a row or column from a
+              GeoDataFrame, the goal is to only return a GeoSeries for a
+              geometry column, and not return a GeoSeries for a row that happened
+              to come from a DataFrame with only geometry dtype columns (and
+              thus could have a geometry dtype). Therefore, we don't return a
+              GeoSeries if we are sure we are in a row selection case (by
+              checking the identity of the index)
+            """
             srs = pd.Series(data, index, **kwargs)
             is_row_proxy = srs.index is self.columns
             if is_geometry_type(srs) and not is_row_proxy:
