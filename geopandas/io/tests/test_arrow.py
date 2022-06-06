1645d975efe173e1909c31744afdabae8de5fe5f
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2022-06-06 10:07:39 +0200

    update test file to use dummy data

--- geopandas/io/tests/test_arrow.py
+++ geopandas/io/tests/test_arrow.py
@@ -722,22 +722,29 @@ def test_read_gdal_files():
     Since it is currently not yet straightforward to install GDAL with
     Parquet/Arrow enabled in our conda setup, we are testing with some
     generated files included in the repo (using GDAL 3.5.0):
-    df = geopandas.read_file(geopandas.datasets.get_path('naturalearth_lowres'))
-    df.head(2).to_file("naturalearth_lowres_top2.gpkg", GEOMETRY_NAME="geometry")
+
+    # small dummy test dataset (not naturalearth_lowres, as this can change over time)
+    from shapely.geometry import box, MultiPolygon
+    df = geopandas.GeoDataFrame(
+        {"col_str": ["a", "b"], "col_int": [1, 2], "col_float": [0.1, 0.2]},
+        geometry=[MultiPolygon([box(0, 0, 1, 1), box(2, 2, 3, 3)]), box(4, 4, 5,5)],
+        crs="EPSG:4326",
+    )
+    df.to_file("test_data.gpkg", GEOMETRY_NAME="geometry")
     and then the gpkg file is converted to Parquet/Arrow with:
-    $ ogr2ogr -f Parquet -lco FID= naturalearth_lowres_top2_gdal350.parquet naturalearth_lowres_top2.gpkg  # noqa: E501
-    $ ogr2ogr -f Arrow -lco FID= -lco GEOMETRY_ENCODING=WKB naturalearth_lowres_top2_gdal350.arrow naturalearth_lowres_top2.gpkg  # noqa: E501
+    $ ogr2ogr -f Parquet -lco FID= test_data_gdal350.parquet test_data.gpkg
+    $ ogr2ogr -f Arrow -lco FID= -lco GEOMETRY_ENCODING=WKB test_data_gdal350.arrow test_data.gpkg  # noqa: E501
     """
     check_crs = Version(pyproj.__version__) >= Version("3.0.0")
 
-    expected = geopandas.read_file(get_path("naturalearth_lowres")).head(2)
-
-    df = geopandas.read_parquet(
-        DATA_PATH / "arrow" / "naturalearth_lowres_top2_gdal350.parquet"
+    expected = geopandas.GeoDataFrame(
+        {"col_str": ["a", "b"], "col_int": [1, 2], "col_float": [0.1, 0.2]},
+        geometry=[MultiPolygon([box(0, 0, 1, 1), box(2, 2, 3, 3)]), box(4, 4, 5, 5)],
+        crs="EPSG:4326",
     )
+
+    df = geopandas.read_parquet(DATA_PATH / "arrow" / "test_data_gdal350.parquet")
     assert_geodataframe_equal(df, expected, check_crs=check_crs)
 
-    df = geopandas.read_feather(
-        DATA_PATH / "arrow" / "naturalearth_lowres_top2_gdal350.arrow"
-    )
+    df = geopandas.read_feather(DATA_PATH / "arrow" / "test_data_gdal350.arrow")
     assert_geodataframe_equal(df, expected, check_crs=check_crs)
