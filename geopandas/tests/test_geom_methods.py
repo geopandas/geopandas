1a2013c20efce6262ef3c5f9a63b0f5692ef9512
SimoParmeg <parmeggiani.simone@gmail.com>
2022-09-20 16:18:47 +0200

    typo fixing

--- geopandas/tests/test_geom_methods.py
+++ geopandas/tests/test_geom_methods.py
@@ -681,7 +681,7 @@ class TestGeomMethods:
 
     @pytest.mark.skipif(
         not compat.SHAPELY_GE_18,
-        reason="ignore_index keyword introduced in shapely 1.8.0",
+        reason="make_valid keyword introduced in shapely 1.8.0",
     )
     def test_make_valid(self):
         polygon1 = Polygon([(0, 0), (0, 2), (1, 1), (2, 2), (2, 0), (1, 1), (0, 0)])
