48bdba81121625b8e6b4996369078bc989d66b70
rraymondgh <rraymondgh@users.noreply.github.com>
2022-11-05 23:44:41 +0000

    [Bot] Update Versioneer

--- geopandas/__init__.py
+++ geopandas/__init__.py
@@ -30,3 +30,7 @@ __version__ = _version.get_versions()["version"]
 from . import _version
 
 __version__ = _version.get_versions()["version"]
+
+from . import _version
+
+__version__ = _version.get_versions()["version"]
