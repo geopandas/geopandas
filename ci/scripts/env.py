ce35312f54e2a2274b35d3a50b7c1546da22563d
rraymondgh <rob.j.raymond@gmail.com>
2022-11-25 14:48:33 +0000

    a few more changes

--- ci/scripts/env.py
+++ ci/scripts/env.py
@@ -5,9 +5,9 @@ locks = list(Path.home().joinpath("geopandas/ci/lock").glob("*.lock"))
 envs = list(Path.home().joinpath("geopandas/ci/envs").glob("*.yaml"))
 
 d = [
-    {"env": str(Path(*fn.parts[-3:])), "env_name": fn.stem}
+    {"file": str(Path(*fn.parts[-3:])), "name": fn.stem}
     for fn in locks + [e for e in envs if e.stem not in [l.stem for l in locks]]
-    # if fn.suffix == ".yaml"
+    if fn.suffix == ".lock"
 ]
-# d = d[0:1]
+d = d[0:1]
 print(f"::set-output name=env::{json.dumps(d)}")
