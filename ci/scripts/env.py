1113e68be3ae804938ce43beab50747801588cc8
rraymondgh <rob.j.raymond@gmail.com>
2022-12-07 19:45:54 +0000

    first real working version

--- ci/scripts/env.py
+++ ci/scripts/env.py
@@ -1,13 +1,62 @@
-import json
+import json, os, random
 from pathlib import Path
 
-locks = list(Path.home().joinpath("geopandas/ci/lock").glob("*.lock"))
-envs = list(Path.home().joinpath("geopandas/ci/envs").glob("*.yaml"))
+# is runtime env nektos/act?
+ACT = "ACT" in os.environ and os.environ["ACT"] == "true"
+ADD_INCLUDE = json.loads(os.environ["ADD_INCLUDE"]) if "ADD_INCLUDE" in os.environ else True
+ENV_YAML = os.environ["ENV_YAML"] if "ENV_YAML" in os.environ else "*.yaml"
 
-d = [
+# special case development envs
+dev = ["310-dev"]
+
+# environment that are tested on macos and windows as well
+all_os = ["38-latest-conda-forge", "39-latest-conda-forge"]
+
+# need the name for conda activation and filename for building
+# conda env
+env = [
     {"file": str(Path(*fn.parts[-3:])), "name": fn.stem}
-    for fn in locks + [e for e in envs if e.stem not in [l.stem for l in locks]]
-    if fn.suffix == ".lock"
+    for fn in Path.cwd().joinpath("ci/envs").glob(ENV_YAML)
+    if fn.stem not in dev
 ]
-d = d[0:1]
-print(f"::set-output name=env::{json.dumps(d)}")
+
+# add macos and windows environments to the matrix
+inc = [
+    {"os": runner, "env": e, "postgis": False, "dev": False}
+    for e in env
+    if e["name"] in all_os
+    for runner in ["windows-latest", "macos-latest"]
+]
+
+# add the dev environments
+inc += [
+    {"os": "ubuntu-latest", "env": e, "postgis": False, "dev": True}
+    for e in [{"name": n, "file": f"ci/envs/{n}.yaml"} for n in dev]
+]
+
+matrix = {
+    "env": env,
+    "os": ["ubuntu-latest"],
+    "postgis": [False],
+    "dev": [False],
+    # "include": inc,
+}
+if ADD_INCLUDE:
+    matrix["include"] = inc
+
+if ACT:
+    opts = {
+        "miniforge-version": "",
+        "miniforge-variant": "",
+        "run-post": "false",
+    }
+else:
+    opts = {
+        "miniforge-version": "latest",
+        "miniforge-variant": "Mambaforge",
+        "run-post": "true",
+    }
+
+print(f"opts={json.dumps(opts)}")
+print(f"matrix={json.dumps(matrix)}")
+
