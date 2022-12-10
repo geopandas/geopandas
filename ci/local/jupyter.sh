227d676d359482f0669bd136f816248c2a4e6b87
rraymondgh <rob.j.raymond@gmail.com>
2022-12-10 12:34:35 +0000

    improve jupyter integration and a couple of fixes

--- ci/local/jupyter.sh
+++ ci/local/jupyter.sh
@@ -1,1 +1,15 @@
-jupyter lab --ip='*' --NotebookApp.token='' --NotebookApp.password=''
+source /home/vagrant/miniconda3/etc/profile.d/conda.sh
+# install kernel into each env if required, skip base env
+for e in `conda env list --json | jq --raw-output .envs[1:][]`
+do 
+    e=`basename $e`
+
+    if [[ $( conda list -n $e -f ipython --json | jq --raw-output .[].name ) != "ipython" ]]
+    then
+        mamba install -n $e ipykernel
+        conda activate $e
+        ipython kernel install --user --name=$e
+        conda deactivate
+    fi
+done
+jupyter lab --ip='*' --IdentityProvider.token='' --IdentityProvider.password=''
