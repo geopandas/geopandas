03896f098ab20c8f6e3fc0847a5788af80f22ee8
Martin Fleischmann <martin@martinfleischmann.net>
2022-12-30 08:09:04 +0100

    TST: bring back append test for pandas<2.0.0 (#2648)
    Co-authored-by: Joris Van den Bossche <jorisvandenbossche@gmail.com>

--- geopandas/_compat.py
+++ geopandas/_compat.py
@@ -22,6 +22,7 @@ PANDAS_GE_13 = Version(pd.__version__) >= Version("1.3.0")
 PANDAS_GE_14 = Version(pd.__version__) >= Version("1.4.0rc0")
 PANDAS_GE_20 = Version(pd.__version__) >= Version("2.0.0.dev0")
 
+
 # -----------------------------------------------------------------------------
 # Shapely / PyGEOS compat
 # -----------------------------------------------------------------------------
