23ee3a3dcdcd9931c7e56f7f3bef5a7deb10b052
Brett Tully <brett.tully@gmail.com>
2023-01-05 17:14:08 +1000

    Added test case

--- geopandas/tests/test_overlay.py
+++ geopandas/tests/test_overlay.py
@@ -4,6 +4,7 @@ from packaging.version import Version
 import numpy as np
 import pandas as pd
 
+import shapely
 from shapely.geometry import Point, Polygon, LineString, GeometryCollection, box
 
 import geopandas
@@ -813,6 +814,26 @@ def test_no_intersection():
     expected = GeoDataFrame(columns=["foo", "bar", "geometry"])
     result = overlay(gdf1, gdf2, how="intersection")
     assert_geodataframe_equal(result, expected, check_index_type=False)
+
+
+@pytest.mark.skipif(not _compat.SHAPELY_GE_20, reason="requires shapely 2 or higher")
+@pytest.mark.parametrize("snap_to_int", [True, False])
+def test_overlay_difference_precision(snap_to_int):
+    old_use_pygeos = _compat.USE_PYGEOS
+    if old_use_pygeos:
+        _compat.set_use_pygeos(False)
+    try:
+        gdf = read_file(
+            os.path.join(DATA, "overlay_difference_precision", "gdf.geojson.zip")
+        )
+        if snap_to_int:
+            gdf = gdf.set_geometry(
+                shapely.set_precision(gdf.geometry, 1), inplace=False
+            )
+        result = overlay(gdf, gdf, how="difference", keep_geom_type=True)
+        assert result.is_valid.all()
+    finally:
+        _compat.set_use_pygeos(old_use_pygeos)
 
 
 class TestOverlayWikiExample:
