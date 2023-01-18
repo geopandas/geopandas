1e1e6fe330379f660aab20f4ee01bcc0c432897f
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2023-01-18 10:44:27 +0000

    Add caching pyproj transform from crs
    Implementing recommended optimisations as per pyproj 3 https://pyproj4.github.io/pyproj/stable/advanced_examples.html#caching-pyproj-objects

---
+++ geopandas/_transformer.py
@@ -0,0 +1,13 @@
+from pyproj import Transformer as PyprojTransformer
+from pyproj import __version__ as pyproj_version
+
+# Setup cached Transformer for pyproj 3.x
+if pyproj_version.startswith("3"):
+    from functools import lru_cache
+
+    TransformerFromCRS = lru_cache(PyprojTransformer.from_crs)
+else:
+    TransformerFromCRS = PyprojTransformer.from_crs
+
+
+__all__ = ["TransformerFromCRS"]
