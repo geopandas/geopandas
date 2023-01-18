98102390c18b0d80e3a8154b092f15549be3f3bb
mblackgeo <18327836+mblackgeo@users.noreply.github.com>
2023-01-18 11:08:03 +0000

    Cache transformer for pyproj 2 and 3

--- geopandas/_transformer.py
+++ geopandas/_transformer.py
@@ -1,8 +1,8 @@
 from pyproj import Transformer as PyprojTransformer
 from pyproj import __version__ as pyproj_version
 
-# Setup cached Transformer for pyproj 3.x
-if pyproj_version.startswith("3"):
+# Setup cached Transformer for pyproj 2.x and 3.x
+if pyproj_version.startswith(("2", "3")):
     from functools import lru_cache
 
     TransformerFromCRS = lru_cache(PyprojTransformer.from_crs)
