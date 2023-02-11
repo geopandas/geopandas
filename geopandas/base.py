cb7cc1bd698605ae71b400f95a79bccd5a32b27c
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2023-02-11 11:37:10 +0100

    REF: rename GeometryArray.data to _data

--- geopandas/base.py
+++ geopandas/base.py
@@ -50,7 +50,7 @@ def _binary_geo(op, this, other, align):
     from .geoseries import GeoSeries
 
     geoms, index = _delegate_binary_method(op, this, other, align)
-    return GeoSeries(geoms.data, index=index, crs=this.crs)
+    return GeoSeries(geoms, index=index, crs=this.crs)
 
 
 def _binary_op(op, this, other, align, *args, **kwargs):
@@ -67,7 +67,7 @@ def _delegate_property(op, this):
     if isinstance(data, GeometryArray):
         from .geoseries import GeoSeries
 
-        return GeoSeries(data.data, index=this.index, crs=this.crs)
+        return GeoSeries(data, index=this.index, crs=this.crs)
     else:
         return Series(data, index=this.index)
 
@@ -78,7 +78,7 @@ def _delegate_geo_method(op, this, *args, **kwargs):
     from .geoseries import GeoSeries
 
     a_this = GeometryArray(this.geometry.values)
-    data = getattr(a_this, op)(*args, **kwargs).data
+    data = getattr(a_this, op)(*args, **kwargs)
     return GeoSeries(data, index=this.index, crs=this.crs)
 
 
@@ -2709,7 +2709,7 @@ GeometryCollection
 
         geometry_array = GeometryArray(self.geometry.values)
         clipped_geometry = geometry_array.clip_by_rect(xmin, ymin, xmax, ymax)
-        return GeoSeries(clipped_geometry.data, index=self.index, crs=self.crs)
+        return GeoSeries(clipped_geometry, index=self.index, crs=self.crs)
 
     #
     # Other operations
--- geopandas/geoseries.py
+++ geopandas/geoseries.py
@@ -874,13 +874,13 @@ class GeoSeries(GeoPandasBase, Series):
         if compat.USE_SHAPELY_20 or (compat.USE_PYGEOS and compat.PYGEOS_GE_09):
             if compat.USE_SHAPELY_20:
                 geometries, outer_idx = shapely.get_parts(
-                    self.values.data, return_index=True
+                    self.values._data, return_index=True
                 )
             else:
                 import pygeos  # noqa
 
                 geometries, outer_idx = pygeos.get_parts(
-                    self.values.data, return_index=True
+                    self.values._data, return_index=True
                 )
 
             if len(outer_idx):
