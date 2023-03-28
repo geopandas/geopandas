b71a3bf261df690d58eeb4caa528ebddd31345a7
Isaac Boates <iboates@gmail.com>
2023-03-28 20:34:38 +0200

    Made read_postgis raise ValueError if the geom_col is specified twice (current error is cryptic)

--- geopandas/io/tests/test_sql.py
+++ geopandas/io/tests/test_sql.py
@@ -698,10 +698,13 @@ class TestIO:
         with pytest.raises(ValueError, match="CRS of the target table"):
             write_postgis(df_nybb2, con=engine, name=table, if_exists="append")
 
-    def test_duplicate_geometry_column_fails(self, engine_postgis, df_nybb):
+    def test_duplicate_geometry_column_fails(self, engine_postgis):
         """
-        Tests that a ValueError is raised if an SQL query returns two
+        Tests that a ValueError is raised if an SQL query returns two geometry columns.
         """
         engine = engine_postgis
+
+        sql = "select ST_MakePoint(0, 0) as geom, ST_MakePoint(0, 0) as geom;"
+
         with pytest.raises(ValueError):
-            read_postgis("select ST_MakePoint(0, 0) as geom, ST_MakePoint(0, 0) as geom", engine, geom_col="geom")
+            read_postgis(sql, engine, geom_col="geom")
