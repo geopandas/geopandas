a7bd015082da3d655a0fd62ac3abd069fd3b86a5
Matt Richards <mrichards7@outlook.com.au>
2023-04-02 10:07:01 +1000

    make kwargs kwarg only so I can safely remove them

--- geopandas/tests/test_op_output_types.py
+++ geopandas/tests/test_op_output_types.py
@@ -83,7 +83,7 @@ def assert_object(
 
 
 def assert_obj_none(
-    result, expected_type, geo_name="geometry", crs=crs_wgs, check_none_name=False
+    result, expected_type, *, geo_name="geometry", crs=crs_wgs, check_none_name=False
 ):
     if expected_type == GeoDataFrame:
         if geo_name is not None:
@@ -114,8 +114,8 @@ def test_getitem(df):
     assert_object(df[["value1", "value2"]], pd.DataFrame)
     assert_object(df[[geo_name, "geometry2"]], GeoDataFrame, geo_name)
     assert_object(df[[geo_name]], GeoDataFrame, geo_name)
-    assert_obj_none(df[["geometry2", "value1"]], GeoDataFrame, None)
-    assert_obj_none(df[["geometry2"]], GeoDataFrame, None)
+    assert_obj_none(df[["geometry2", "value1"]], GeoDataFrame, geo_name=None)
+    assert_obj_none(df[["geometry2"]], GeoDataFrame, geo_name=None)
     assert_object(df[["value1"]], pd.DataFrame)
     # Series
     assert_object(df[geo_name], GeoSeries, geo_name)
@@ -128,8 +128,8 @@ def test_loc(df):
     assert_object(df.loc[:, ["value1", "value2"]], pd.DataFrame)
     assert_object(df.loc[:, [geo_name, "geometry2"]], GeoDataFrame, geo_name)
     assert_object(df.loc[:, [geo_name]], GeoDataFrame, geo_name)
-    assert_obj_none(df.loc[:, ["geometry2", "value1"]], GeoDataFrame, None)
-    assert_obj_none(df.loc[:, ["geometry2"]], GeoDataFrame, None)
+    assert_obj_none(df.loc[:, ["geometry2", "value1"]], GeoDataFrame, geo_name=None)
+    assert_obj_none(df.loc[:, ["geometry2"]], GeoDataFrame, geo_name=None)
     assert_object(df.loc[:, ["value1"]], pd.DataFrame)
     # Series
     assert_object(df.loc[:, geo_name], GeoSeries, geo_name)
@@ -142,8 +142,8 @@ def test_iloc(df):
     assert_object(df.iloc[:, 0:2], pd.DataFrame)
     assert_object(df.iloc[:, 2:4], GeoDataFrame, geo_name)
     assert_object(df.iloc[:, [2]], GeoDataFrame, geo_name)
-    assert_obj_none(df.iloc[:, [3, 0]], GeoDataFrame, None)
-    assert_obj_none(df.iloc[:, [3]], GeoDataFrame, None)
+    assert_obj_none(df.iloc[:, [3, 0]], GeoDataFrame, geo_name=None)
+    assert_obj_none(df.iloc[:, [3]], GeoDataFrame, geo_name=None)
     assert_object(df.iloc[:, [0]], pd.DataFrame)
     # Series
     assert_object(df.iloc[:, 2], GeoSeries, geo_name)
@@ -175,8 +175,10 @@ def test_reindex(df):
     assert_object(df.reindex(columns=[geo_name, "geometry2"]), GeoDataFrame, geo_name)
     assert_object(df.reindex(columns=[geo_name]), GeoDataFrame, geo_name)
     assert_object(df.reindex(columns=["new_col", geo_name]), GeoDataFrame, geo_name)
-    assert_obj_none(df.reindex(columns=["geometry2", "value1"]), GeoDataFrame, None)
-    assert_obj_none(df.reindex(columns=["geometry2"]), GeoDataFrame, None)
+    assert_obj_none(
+        df.reindex(columns=["geometry2", "value1"]), GeoDataFrame, geo_name=None
+    )
+    assert_obj_none(df.reindex(columns=["geometry2"]), GeoDataFrame, geo_name=None)
     assert_object(df.reindex(columns=["value1"]), pd.DataFrame)
 
     # reindexing the rows always preserves the GeoDataFrame
@@ -195,8 +197,10 @@ def test_drop(df):
     assert_object(df.drop(columns=["value1", "value2"]), GeoDataFrame, geo_name)
     cols = ["value1", "value2", "geometry2"]
     assert_object(df.drop(columns=cols), GeoDataFrame, geo_name)
-    assert_obj_none(df.drop(columns=[geo_name, "value2"]), GeoDataFrame, None)
-    assert_obj_none(df.drop(columns=["value1", "value2", geo_name]), GeoDataFrame, None)
+    assert_obj_none(df.drop(columns=[geo_name, "value2"]), GeoDataFrame, geo_name=None)
+    assert_obj_none(
+        df.drop(columns=["value1", "value2", geo_name]), GeoDataFrame, geo_name=None
+    )
     assert_object(df.drop(columns=["geometry2", "value2", geo_name]), pd.DataFrame)
 
 
@@ -211,9 +215,14 @@ def test_apply(df):
     assert_object(df[[geo_name, "geometry2"]].apply(identity), GeoDataFrame, geo_name)
     assert_object(df[[geo_name]].apply(identity), GeoDataFrame, geo_name)
     assert_obj_none(
-        df[["geometry2", "value1"]].apply(identity), GeoDataFrame, None, None
+        df[["geometry2", "value1"]].apply(identity),
+        GeoDataFrame,
+        geo_name=None,
+        crs=None,
     )
-    assert_obj_none(df[["geometry2"]].apply(identity), GeoDataFrame, None, None)
+    assert_obj_none(
+        df[["geometry2"]].apply(identity), GeoDataFrame, geo_name=None, crs=None
+    )
     assert_object(df[["value1"]].apply(identity), pd.DataFrame)
 
     # axis = 0, Series
@@ -244,7 +253,9 @@ def test_apply_axis1_secondary_geo_cols(df):
     def identity(x):
         return x
 
-    assert_obj_none(df[["geometry2"]].apply(identity, axis=1), GeoDataFrame, None, None)
+    assert_obj_none(
+        df[["geometry2"]].apply(identity, axis=1), GeoDataFrame, geo_name=None, crs=None
+    )
 
 
 def test_expanddim_in_apply():
@@ -270,9 +281,9 @@ def test_expandim_in_groupby_aggregate_multiple_funcs():
 
     grouped = s.groupby([0, 1, 0])
     agg = grouped.agg([total_area, union])
-    assert_obj_none(agg, GeoDataFrame, None, None, check_none_name=True)
+    assert_obj_none(agg, GeoDataFrame, geo_name=None, crs=None, check_none_name=True)
     result = grouped.agg([union, total_area])
-    assert_obj_none(result, GeoDataFrame, None, None, check_none_name=True)
+    assert_obj_none(result, GeoDataFrame, geo_name=None, crs=None, check_none_name=True)
     assert_object(grouped.agg([total_area, total_area]), pd.DataFrame)
     assert_object(grouped.agg([total_area]), pd.DataFrame)
 
@@ -285,7 +296,9 @@ def test_expanddim_in_unstack():
         index=pd.MultiIndex.from_tuples([("A", "a"), ("A", "b"), ("B", "a")]),
     )
     unstack = s.unstack()
-    assert_obj_none(unstack, GeoDataFrame, None, None, False)
+    assert_obj_none(
+        unstack, GeoDataFrame, geo_name=None, crs=None, check_none_name=False
+    )
 
     if compat.PANDAS_GE_12:
         assert unstack._geometry_column_name is None
@@ -295,7 +308,7 @@ def test_expanddim_in_unstack():
     # https://github.com/geopandas/geopandas/issues/2486
     s.name = "geometry"
     unstack = s.unstack()
-    assert_obj_none(unstack, GeoDataFrame, None, None)
+    assert_obj_none(unstack, GeoDataFrame, geo_name=None, crs=None)
 
 
 # indexing /  constructor_sliced tests
