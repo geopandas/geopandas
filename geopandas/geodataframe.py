ac07e3c5ff79cb7c3e532de97db661beb61e24b6
Reslan Tinawi <reslan3121@gmail.com>
2023-05-19 17:50:42 +0300

    Remove `@doc` decorator from `explore` method
    The `explore` method already has a `docstring`.  Decorating the `explore` methods with `@doc` docerator is causing the documentation to show the docstring twice (here: https://geopandas.org/en/stable/docs/reference/api/geopandas.GeoDataFrame.explore.html)

--- geopandas/geodataframe.py
+++ geopandas/geodataframe.py
@@ -2095,8 +2095,7 @@ individually so that features may have different properties
         return self.geometry.difference(other)
 
     plot = CachedAccessor("plot", geopandas.plotting.GeoplotAccessor)
-
-    @doc(_explore)
+    
     def explore(self, *args, **kwargs):
         """Interactive map based on folium/leaflet.js"""
         return _explore(self, *args, **kwargs)
