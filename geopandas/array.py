79aad7d706c38c129b739eda9fb6e5498bab71f0
“James Gardiner@ <james@jgardiner.co.uk>
2023-05-27 20:29:03 +0100

    chore: update method signature and docs to explicitly list shapely params

--- geopandas/array.py
+++ geopandas/array.py
@@ -525,9 +525,16 @@ class GeometryArray(ExtensionArray):
     def exterior(self):
         return GeometryArray(vectorized.exterior(self._data), crs=self.crs)
 
-    def offset_curve(self, distance, **kwargs):
+    def offset_curve(self, distance, quad_segs=16, join_style="round", mitre_limit=5.0):
         return GeometryArray(
-            vectorized.offset_curve(self._data, distance, **kwargs), crs=self.crs
+            vectorized.offset_curve(
+                self._data,
+                distance,
+                quad_segs=quad_segs,
+                join_style=join_style,
+                mitre_limit=mitre_limit,
+            ),
+            crs=self.crs,
         )
 
     @property
