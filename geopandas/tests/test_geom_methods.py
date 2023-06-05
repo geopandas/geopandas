15ee81414dcf66885955a93ba7743d119853cc97
“James Gardiner@ <james@jgardiner.co.uk>
2023-06-05 17:03:25 +0100

    chore: test with polygons

--- geopandas/tests/test_geom_methods.py
+++ geopandas/tests/test_geom_methods.py
@@ -794,8 +794,41 @@ class TestGeomMethods:
         not (compat.USE_PYGEOS or compat.USE_SHAPELY_20),
         reason="segmentize keyword introduced in shapely 2.0",
     )
-    def test_segmentize(self):
-        expected = GeoSeries(
+    def test_segmentize_linestrings(self):
+        expected_g1 = GeoSeries(
+            [
+                Polygon(
+                    (
+                        (
+                            (0, 0),
+                            (0.5, 0),
+                            (1, 0),
+                            (1, 0.5),
+                            (1, 1),
+                            (0.6666666666666666, 0.6666666666666666),
+                            (0.3333333333333333, 0.3333333333333333),
+                            (0, 0),
+                        )
+                    )
+                ),
+                Polygon(
+                    (
+                        (
+                            (0, 0),
+                            (0.5, 0),
+                            (1, 0),
+                            (1, 0.5),
+                            (1, 1),
+                            (0.5, 1),
+                            (0, 1),
+                            (0, 0.5),
+                            (0, 0),
+                        )
+                    )
+                ),
+            ]
+        )
+        expected_g5 = GeoSeries(
             [
                 LineString([(0, 0), (0, 0.5), (0, 1), (0.5, 1), (1, 1)]),
                 LineString(
@@ -803,8 +836,10 @@ class TestGeomMethods:
                 ),
             ]
         )
-        result = self.g5.segmentize(max_segment_length=0.5)
-        assert_geoseries_equal(expected, result)
+        result_g1 = self.g1.segmentize(max_segment_length=0.5)
+        result_g5 = self.g5.segmentize(max_segment_length=0.5)
+        assert_geoseries_equal(expected_g1, result_g1)
+        assert_geoseries_equal(expected_g5, result_g5)
 
     def test_convex_hull(self):
         # the convex hull of a square should be the same as the square
