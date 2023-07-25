1c4b7dadc0e22518e36807be3bf5b75f3cbd6923
Martin Fleischmann <martin@martinfleischmann.net>
2023-07-25 09:17:22 +0200

    Update geopandas/geodataframe.py

--- geopandas/geodataframe.py
+++ geopandas/geodataframe.py
@@ -1699,8 +1699,9 @@ individually so that features may have different properties
         by : str or list-like, default None
             Column(s) whose values define the groups to be dissolved. If None,
             the entire GeoDataFrame is considered as a single group. If a list-like
-            object is provided, the values in the list are treated as categorical labels,
-            and polygons will be combined based on the equality of these categorical labels.
+            object is provided, the values in the list are treated as categorical
+            labels, and polygons will be combined based on the equality of
+            these categorical labels.
         aggfunc : function or string, default "first"
             Aggregation function for manipulation of data associated
             with each group. Passed to pandas `groupby.agg` method.
