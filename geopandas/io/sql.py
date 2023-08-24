1f88aea38f2e1f69912dce0133596c346f2da902
James Nunn <2095017+jamesnunn@users.noreply.github.com>
2023-08-24 13:58:06 +0100

    Don't overwrite user-supplied geometry dtype
    When writing to PostGIS, I want the option to allow mixed geometries in a table. The current behaviour of `to_postgis` implicitly disallows this by setting the geometry type to whatever the first dataframe type is to populate that table.
    
    This change simply looks to see if the user has defined the geometry dtype first. If not then it continues with the original behaviour of setting the geometry type to the dataframe, otherwise it leaves the dtype untouched and allows the user to specify the target table geometry type.

--- geopandas/io/sql.py
+++ geopandas/io/sql.py
@@ -419,10 +419,11 @@ def _write_postgis(
 
     # Build dtype with Geometry
     if dtype is not None:
-        dtype[geom_name] = Geometry(geometry_type=geometry_type, srid=srid)
+        if geom_name not in dtype:
+            dtype[geom_name] = Geometry(geometry_type=geometry_type, srid=srid)
     else:
         dtype = {geom_name: Geometry(geometry_type=geometry_type, srid=srid)}
-
+        
     # Convert LinearRing geometries to LineString
     if has_curve:
         gdf = _convert_linearring_to_linestring(gdf, geom_name)
