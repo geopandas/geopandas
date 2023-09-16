a095566f95a97e398667c8a08f951a9c6a4c696c
Matt Richards <mrichards7@outlook.com.au>
2023-09-16 11:24:16 +1000

    remove overloads

--- geopandas/geodataframe.py
+++ geopandas/geodataframe.py
@@ -1955,55 +1955,6 @@ individually so that features may have different properties
 
         return df
 
-    # overrides the pandas astype method to ensure the correct return type
-    def astype(self, dtype, copy=True, errors="raise", **kwargs):
-        """
-        Cast a pandas object to a specified dtype ``dtype``.
-
-        Returns a GeoDataFrame when the geometry column is kept as geometries,
-        otherwise returns a pandas DataFrame.
-
-        See the pandas.DataFrame.astype docstring for more details.
-
-        Returns
-        -------
-        GeoDataFrame or DataFrame
-        """
-        df = super().astype(dtype, copy=copy, errors=errors, **kwargs)
-
-        try:
-            geoms = df[self._geometry_column_name]
-            if is_geometry_type(geoms):
-                return geopandas.GeoDataFrame(df, geometry=self._geometry_column_name)
-        except KeyError:
-            pass
-        # if the geometry column is converted to non-geometries or did not exist
-        # do not return a GeoDataFrame
-        return pd.DataFrame(df)
-
-    def convert_dtypes(self, *args, **kwargs):
-        """
-        Convert columns to best possible dtypes using dtypes supporting ``pd.NA``.
-
-        Always returns a GeoDataFrame as no conversions are applied to the
-        geometry column.
-
-        See the pandas.DataFrame.convert_dtypes docstring for more details.
-
-        Returns
-        -------
-        GeoDataFrame
-
-        """
-        # Overridden to fix GH1870, that return type is not preserved always
-        # (and where it was, geometry col was not)
-
-        return GeoDataFrame(
-            super().convert_dtypes(*args, **kwargs),
-            geometry=self.geometry.name,
-            crs=self.crs,
-        )
-
     def to_postgis(
         self,
         name,
