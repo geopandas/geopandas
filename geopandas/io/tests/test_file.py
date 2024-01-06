bddb6beebc7e96675226c3ae402b5534af62e8b3
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2024-01-06 16:25:21 +0100

    TST: update new url for nybb dataset (#3134)

--- geopandas/io/tests/test_file.py
+++ geopandas/io/tests/test_file.py
@@ -578,7 +578,7 @@ def test_read_file(engine):
         "main/geopandas/tests/data/null_geom.geojson",
         # url to zip file
         "https://raw.githubusercontent.com/geopandas/geopandas/"
-        "main/geopandas/datasets/nybb_16a.zip",
+        "main/geopandas/tests/data/nybb_16a.zip",
         # url to zipfile without extension
         "https://geonode.goosocean.org/download/480",
         # url to web service
