b2b29d979ee9e1e97e01c23f4b9564215fcbbf20
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2024-02-09 11:21:33 +0100

    add basic test for GeoSeries constructor with copy=False as well

--- geopandas/_compat.py
+++ geopandas/_compat.py
@@ -14,8 +14,8 @@ PANDAS_GE_14 = Version(pd.__version__) >= Version("1.4.0rc0")
 PANDAS_GE_15 = Version(pd.__version__) >= Version("1.5.0")
 PANDAS_GE_20 = Version(pd.__version__) >= Version("2.0.0")
 PANDAS_GE_21 = Version(pd.__version__) >= Version("2.1.0")
-PANDAS_GE_22 = Version(pd.__version__) >= Version("2.2.0.dev0")
-
+PANDAS_GE_22 = Version(pd.__version__) >= Version("2.2.0")
+PANDAS_GE_30 = Version(pd.__version__) >= Version("3.0.0.dev0")
 
 # -----------------------------------------------------------------------------
 # Shapely / GEOS compat
