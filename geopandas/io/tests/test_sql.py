6fa26a9a2abd90ac0bb162b74b1e3493453a3496
Alec Johnson <35614706+MrAlecJohnson@users.noreply.github.com>
2024-03-06 15:34:36 +0000

    Update geopandas/io/tests/test_sql.py
    Co-authored-by: Matt Richards <45483497+m-richards@users.noreply.github.com>

--- geopandas/io/tests/test_sql.py
+++ geopandas/io/tests/test_sql.py
@@ -52,7 +52,7 @@ def connection_psycopg_any(psycopg_package_name: str):
             "ignore", message="pandas only supports SQLAlchemy connectable.*"
         )
         yield con
-        con.close()
+    con.close()
 
 
 def engine_psycopg_any(psycopg_package_name: str):
