795d154bb138d594824db30d457d37b854204461
Martin Fleischmann <martin@martinfleischmann.net>
2024-04-02 17:35:01 +0200

    re-sort within query

--- geopandas/tools/sjoin.py
+++ geopandas/tools/sjoin.py
@@ -196,6 +196,9 @@ def _geom_predicate_query(left_df, right_df, predicate, distance):
         # within is implemented as the inverse of contains
         # flip back the results
         r_idx, l_idx = l_idx, r_idx
+        indexer = np.lexsort((r_idx, l_idx))
+        l_idx = l_idx[indexer]
+        r_idx = r_idx[indexer]
 
     return l_idx, r_idx
 
@@ -319,14 +322,14 @@ def _adjust_indexers(indices, distances, original_length, how, predicate):
     """
     # the indices represent an inner join, no adjustment needed
     if how == "inner":
-        if predicate == "within":
-            # except for the within predicate, where we switched to contains
-            # with swapped left/right -> need to re-sort to have consistent result
-            l_idx, r_idx = indices
-            indexer = np.lexsort((r_idx, l_idx))
-            indices = l_idx[indexer], r_idx[indexer]
-            if distances is not None:
-                distances = distances[indexer]
+        # if predicate == "within":
+        #     # except for the within predicate, where we switched to contains
+        #     # with swapped left/right -> need to re-sort to have consistent result
+        #     l_idx, r_idx = indices
+        #     indexer = np.lexsort((r_idx, l_idx))
+        #     indices = l_idx[indexer], r_idx[indexer]
+        #     if distances is not None:
+        #         distances = distances[indexer]
         return indices, distances
 
     l_idx, r_idx = indices
