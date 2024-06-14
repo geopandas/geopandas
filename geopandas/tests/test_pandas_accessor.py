8aa87144eff87bacd415346778bb60be729b1ba7
Tim Swast <swast@google.com>
2024-06-14 10:30:38 -0500

    add missing unit tests

--- geopandas/tests/test_pandas_accessor.py
+++ geopandas/tests/test_pandas_accessor.py
@@ -1,5 +1,7 @@
 """Ensure geopandas methods can be accessed via pandas 'geo' accessor."""
 
+import re
+
 import pandas as pd
 
 from shapely.geometry import Point
@@ -19,6 +21,27 @@ def s():
     )
 
 
+@pytest.fixture
+def s2():
+    return pd.Series(
+        [Point(x, y + 1) for x, y in zip(range(3), range(3))], dtype=GeometryDtype()
+    )
+
+
+def test_series_geo_buffer(s):
+    """Ensure returned geometry values have the expected dtype."""
+    got = s.geo.buffer(0.2)
+    assert isinstance(got.dtype, GeometryDtype)
+
+
+def test_series_geo_distance(s, s2):
+    got = s.geo.distance(s2)
+    pandas.testing.assert_series_equal(
+        got,
+        pd.Series([1.0, 1.0, 1.0]),
+    )
+
+
 def test_series_geo_x(s):
     x = s.geo.x
     pandas.testing.assert_series_equal(
@@ -27,17 +50,19 @@ def test_series_geo_x(s):
     )
 
 
+def test_series_geo_x_attributeerror_for_not_geo_dtype():
+    s = pd.Series([1, 2, 3])
+
+    with pytest.raises(
+        AttributeError,
+        match=re.escape("Can only use .geo accessor with geometry values"),
+    ):
+        s.geo.x
+
+
 def test_series_geo_y(s):
     y = s.geo.y
     pandas.testing.assert_series_equal(
         y,
         pd.Series([0.0, 1.0, 2.0]),
-    )
-
-
-def test_series_geo_distance(s):
-    got = s.geo.distance(s)
-    pandas.testing.assert_series_equal(
-        got,
-        pd.Series([0.0, 0.0, 0.0]),
     )
