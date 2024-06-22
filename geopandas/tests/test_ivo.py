b788668b3313cad12fe2cf450404e5223fc84663
icaka <moofilbg@gmail.com>
2024-06-22 14:41:28 +0200

    Add instrumentation and tests for render_pep440_old

--- geopandas/tests/test_ivo.py
+++ geopandas/tests/test_ivo.py
@@ -7,7 +7,7 @@ import geopandas as gp
 os.sys.path.append('../explore.py')
 os.sys.path.append('../_version.py')
 from geopandas.explore import coverage_tool_tip, _tooltip_popup
-from geopandas._version import coverage_render_pep440_pre, render_pep440_pre
+from geopandas._version import coverage_render_pep440_pre, coverage_render_pep440_old ,render_pep440_pre, render_pep440_old
 
 def print_percentage(flags):
    percentage = sum(flags.values()) / len(flags) 
@@ -16,33 +16,6 @@ def print_percentage(flags):
 def print_coverage(coverage_dict):
     for branch, hit in coverage_dict.items():
         print(f"{branch} was {'hit' if hit else 'not hit'}")
-
-def test_tooltip_popup():
-    data = {
-        'name': ['A', 'B'],
-        'value': [1, 2],
-        'geometry': [Point(1, 2), Point(3, 4)],
-        '__folium_color':['green','yellow']
-    }
-    df = gp.GeoDataFrame(data, crs=4326)
-
-    assert _tooltip_popup('tooltip', False, df) is None
-    assert _tooltip_popup('tooltip', None, df) is None
-    assert _tooltip_popup('tooltip', 0, df) is None
-    
-    res = _tooltip_popup('tooltip', 2, df)
-    assert isinstance(res, folium.GeoJsonTooltip)
-
-    res = _tooltip_popup('popup', True, df)
-    assert isinstance(res, folium.GeoJsonPopup)
-
-    res = _tooltip_popup("popup", "name", df)
-    assert isinstance(res, folium.GeoJsonPopup)
-
-    print()
-    print("tooltip_popup coverage:")
-    print_coverage(coverage_tool_tip)
-    print_percentage(coverage_tool_tip)
 
 def test_render_pep440_pre():
     testDict = {
@@ -73,3 +46,46 @@ def test_render_pep440_pre():
     print("render_pep440_pre coverage:")
     print_coverage(coverage_render_pep440_pre)
     print_percentage(coverage_render_pep440_pre)
+
+def test_render_pep440_old():
+    testDict = {
+        "closest-tag": "v1.2.3",
+        "distance": 0,
+        "dirty": False
+    }
+    assert render_pep440_old(testDict) == "v1.2.3"
+
+    testDict = {
+        "closest-tag": "v1.2.3",
+        "distance": 5,
+        "dirty": False
+    }
+    assert render_pep440_old(testDict) == "v1.2.3.post5"
+
+    testDict = {
+        "closest-tag": "v1.2.3",
+        "distance": 5,
+        "dirty": True
+    }
+    assert render_pep440_old(testDict) == "v1.2.3.post5.dev0"
+
+
+    testDict = {
+        "closest-tag": None,
+        "distance": 7,
+        "dirty": False
+    }
+    assert render_pep440_old(testDict) == "0.post7"
+
+    testDict = {
+        "closest-tag": None,
+        "distance": 7,
+        "dirty": True
+    }
+    assert render_pep440_old(testDict) == "0.post7.dev0"
+
+    print()
+    print("render_pep440_old coverage:")
+    print_coverage(coverage_render_pep440_old)
+    print_percentage(coverage_render_pep440_old)
+
