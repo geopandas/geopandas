a1a7321524a6ef0706b3673e28373bb20b73f91f
Andreas <a.johannessen@student.vu.nl>
2024-06-24 18:40:00 +0200

    Added instrumentation for invisible branches

--- geopandas/_version.py
+++ geopandas/_version.py
@@ -607,6 +607,7 @@ coverage_branches_rgdl = {
     "pieces_closest_tag": False,
     "pieces_short": False,
     "pieces_dirty": False,
+    "invisible_dirty": False,
     "return": False,
 }
 
@@ -631,6 +632,8 @@ def render_git_describe_long(pieces: Dict[str, Any]) -> str:
     if pieces["dirty"]:
         coverage_branches_rgdl["pieces_dirty"] = True
         rendered += "-dirty"
+    else:
+        coverage_branches_rgdl["invisible_dirty"] = True
     coverage_branches_rgdl["return"] = True
     return rendered
 
