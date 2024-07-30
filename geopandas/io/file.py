99951605e45c60f2b46bdab355428c5d6a8593d9
Pieter Roggemans <pieter.roggemans@gmail.com>
2024-07-30 20:13:54 +0200

    use is_object_dtype

--- geopandas/io/file.py
+++ geopandas/io/file.py
@@ -13,7 +13,7 @@ from urllib.parse import uses_netloc, uses_params, uses_relative
 
 import numpy as np
 import pandas as pd
-from pandas.api.types import is_integer_dtype
+from pandas.api.types import is_integer_dtype, is_object_dtype
 
 import shapely
 from shapely.geometry import mapping
@@ -761,7 +761,7 @@ def infer_schema(df):
     }
 
     def convert_type(column, in_type):
-        if in_type is object:
+        if is_object_dtype(in_type):
             return "str"
         if in_type.name.startswith("datetime64"):
             # numpy datetime type regardless of frequency
