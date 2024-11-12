5912a5c30d400baba6b424ea030c102bbfd5def2
Nicholas YS Tan <39729063+nicholas-ys-tan@users.noreply.github.com>
2024-11-12 21:58:23 +1100

    reformat docstring
    Co-authored-by: Martin Fleischmann <martin@martinfleischmann.net>

--- geopandas/plotting.py
+++ geopandas/plotting.py
@@ -517,9 +517,8 @@ def plot_dataframe(
     column : str, np.array, pd.Series, pd.Index (default None)
         The name of the dataframe column, np.array, pd.Series, or pd.Index
         to be plotted. If np.array, pd.Series, or pd.Index are used then it
-        must have same length as dataframe.
-        Values are used to color the plot. Ignored if `color` is
-        also set.
+        must have same length as dataframe. Values are used to color the plot. 
+        Ignored if `color` is also set.
     kind: str
         The kind of plots to produce. The default is to create a map ("geo").
         Other supported kinds of plots from pandas:
