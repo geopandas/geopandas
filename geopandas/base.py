87d3914d024f8a9deb6fc6143c472c6cf40a38e5
Martin Fleischmann <martin@martinfleischmann.net>
2025-04-07 21:27:04 +0200

    fix docstring

--- geopandas/base.py
+++ geopandas/base.py
@@ -5123,7 +5123,7 @@ GeometryCollection
 
         Examples
         --------
-        >>> from shapely.geometry import Point, LineString
+        >>> from shapely.geometry import Polygon
         >>> s = geopandas.GeoSeries(
         ...     [
         ...         Polygon([(0, 0), (1, 1.1), (2, 0), (1.5, 1), (2, 2), (0, 2)]),
