e41aabbaef85bccbe48c799a98288c5730d8439b
Martin Fleischmann <martin@martinfleischmann.net>
2025-12-04 17:06:57 +0100

    checkpoint

--- geopandas/plotting_v2.py
+++ geopandas/plotting_v2.py
@@ -1,4 +1,7 @@
 import numpy as np
+import pandas as pd
+
+import shapely
 
 from ._compat import HAS_MATPLOTLIB
 
@@ -16,6 +19,38 @@ if HAS_MATPLOTLIB:
     Legend.update_default_handler_map(
         {GeoPandasPolyCollection: HandlerPolyCollection()}
     )
+
+
+def _expand_kwargs(kwargs, multiindex):
+    """
+    Most arguments to the plot functions must be a (single) value, or a sequence
+    of values. This function checks each key-value pair in 'kwargs' and expands
+    it (in place) to the correct length/formats with help of 'multiindex', unless
+    the value appears to already be a valid (single) value for the key.
+    """
+    from collections.abc import Iterable
+
+    from matplotlib.colors import is_color_like
+
+    scalar_kwargs = ["marker", "path_effects"]
+    for att, value in kwargs.items():
+        if "color" in att:  # color(s), edgecolor(s), facecolor(s)
+            if is_color_like(value):
+                continue
+        elif "linestyle" in att:  # linestyle(s)
+            # A single linestyle can be 2-tuple of a number and an iterable.
+            if (
+                isinstance(value, tuple)
+                and len(value) == 2
+                and isinstance(value[1], Iterable)
+            ):
+                continue
+        elif att in scalar_kwargs:
+            # For these attributes, only a single value is allowed, so never expand.
+            continue
+
+        if pd.api.types.is_list_like(value):
+            kwargs[att] = np.take(value, multiindex, axis=0)
 
 
 def _PolygonPatch(polygon, **kwargs):
@@ -77,6 +112,9 @@ def _plot_polygon_collection(
     Note that all style keywords, like ``color`` that can be set as an array in
     matplotlib shall be passed directly via kwargs.
 
+    No need to explode geometries to single-parts as _PolygonPatch supports
+    MultiPolygons,
+
     Parameters
     ----------
     ax : _type_
@@ -109,6 +147,70 @@ def _plot_polygon_collection(
     collection = GeoPandasPolyCollection(
         [_PolygonPatch(poly) for poly in geoms], **kwargs
     )
+
+    if values is not None:
+        collection.set_array(np.asarray(values))
+        collection.set_cmap(cmap)
+        if "norm" not in kwargs:
+            collection.set_clim(vmin, vmax)
+
+    ax.add_collection(collection, autolim=autolim)
+    ax.autoscale_view()
+    return collection
+
+
+def _plot_linestring_collection(
+    ax,
+    geoms,
+    values=None,
+    color=None,
+    cmap=None,
+    vmin=None,
+    vmax=None,
+    autolim=True,
+    **kwargs,
+):
+    """Plot a collection of LineString and MultiLineString geometries to `ax`.
+
+    Parameters
+    ----------
+    ax : matplotlib.axes.Axes
+        where shapes will be plotted
+    geoms : a sequence of `N` LineStrings and/or MultiLineStrings (can be
+            mixed)
+    values : a sequence of `N` values, optional
+        Values will be mapped to colors using vmin/vmax/cmap. They should
+        have 1:1 correspondence with the geometries (not their components).
+    color : single color or sequence of `N` colors
+        Cannot be used together with `values`.
+    autolim : bool (default True)
+        Update axes data limits to contain the new geometries.
+
+    Returns
+    -------
+    collection : matplotlib.collections.Collection that was plotted
+    """
+    from matplotlib.collections import LineCollection
+
+    geoms, multiindex = shapely.get_parts(geoms, return_index=True)
+    if values is not None:
+        values = np.take(values, multiindex, axis=0)
+
+    # LineCollection does not accept some kwargs.
+    kwargs = {
+        att: value
+        for att, value in kwargs.items()
+        if att not in ["markersize", "marker"]
+    }
+
+    # Add to kwargs for easier checking below.
+    if color is not None:
+        kwargs["color"] = color
+
+    _expand_kwargs(kwargs, multiindex)
+
+    segments = [np.array(linestring.coords)[:, :2] for linestring in geoms]
+    collection = LineCollection(segments, **kwargs)
 
     if values is not None:
         collection.set_array(np.asarray(values))
