c833ae65cfe35318861916a46eb0399b3722fcd5
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2026-01-27 11:50:25 +0100

    BUG: fix from_wkt/wkb with new pd 3 string dtype and missing values (#3714)

--- geopandas/geoseries.py
+++ geopandas/geoseries.py
@@ -603,7 +603,7 @@ class GeoSeries(GeoPandasBase, Series):
                 data = data.reindex(index)
             else:
                 index = data.index
-            data = data.values
+            data = data.to_numpy(na_value=None)
         return cls(
             from_wkb_or_wkt_function(data, crs=crs, on_invalid=on_invalid),
             index=index,
