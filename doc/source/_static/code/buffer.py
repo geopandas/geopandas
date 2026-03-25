555c25ce973d7fa09bafac8874da9d09896234c6
Adam J. Stewart <ajstewart426@gmail.com>
2026-03-25 11:55:14 +0100

    DOC: add examples of negative buffer
    Signed-off-by: Adam J. Stewart <ajstewart426@gmail.com>

--- doc/source/_static/code/buffer.py
+++ doc/source/_static/code/buffer.py
@@ -17,7 +17,7 @@ s = geopandas.GeoSeries(
     ]
 )
 
-fix, axs = plt.subplots(3, 2, figsize=(12, 12), sharex=True, sharey=True)
+fig, axs = plt.subplots(3, 3, figsize=(12, 8), sharex=True, sharey=True)
 for ax in axs.flatten():
     s.plot(ax=ax)
     ax.set(xticks=[], yticks=[])
@@ -28,14 +28,25 @@ axs[0, 0].set_title("s.buffer(0.2)")
 s.buffer(0.2, resolution=2).plot(ax=axs[0, 1], alpha=0.6)
 axs[0, 1].set_title("s.buffer(0.2, resolution=2)")
 
+s.buffer(-0.2).plot(ax=axs[0, 2], color="white", alpha=0.4)
+axs[0, 2].set_title("s.buffer(-0.2)")
+
 s.buffer(0.2, cap_style="square").plot(ax=axs[1, 0], alpha=0.6)
 axs[1, 0].set_title('s.buffer(0.2, cap_style="square")')
 
 s.buffer(0.2, cap_style="flat").plot(ax=axs[1, 1], alpha=0.6)
 axs[1, 1].set_title('s.buffer(0.2, cap_style="flat")')
 
+s.buffer(-0.2, join_style="mitre").plot(ax=axs[1, 2], color="white", alpha=0.4)
+axs[1, 2].set_title('s.buffer(-0.2, join_style="mitre")')
+
 s.buffer(0.2, join_style="mitre").plot(ax=axs[2, 0], alpha=0.6)
 axs[2, 0].set_title('s.buffer(0.2, join_style="mitre")')
 
 s.buffer(0.2, join_style="bevel").plot(ax=axs[2, 1], alpha=0.6)
 axs[2, 1].set_title('s.buffer(0.2, join_style="bevel")')
+
+s.buffer(-0.2, join_style="bevel").plot(ax=axs[2, 2], color="white", alpha=0.4)
+axs[2, 2].set_title('s.buffer(-0.2, join_style="bevel")')
+
+fig.tight_layout()
