8269458b67fe6df67b058397e4c9ff3c8f69cab4
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2026-04-30 15:27:22 +0200

    REF: add to_wkb method on GeometryArray class

--- geopandas/array.py
+++ geopandas/array.py
@@ -247,9 +247,10 @@ def from_wkb(
 
 def to_wkb(geoms: GeometryArray, hex: bool = False, **kwargs):
     """Convert GeometryArray to a numpy object array of WKB objects."""
+    # keeping for back compat for now, but dispatching to the array method
     if not isinstance(geoms, GeometryArray):
         raise ValueError("'geoms' must be a GeometryArray")
-    return shapely.to_wkb(geoms, hex=hex, **kwargs)
+    return geoms.to_wkb(hex=hex, **kwargs)
 
 
 def from_wkt(
@@ -518,7 +519,7 @@ class GeometryArray(ExtensionArray):
         #         )
 
     def __getstate__(self):
-        return (shapely.to_wkb(self._data), self._crs)
+        return (self.to_wkb(), self._crs)
 
     def __setstate__(self, state):
         if not isinstance(state, dict):
@@ -534,6 +535,10 @@ class GeometryArray(ExtensionArray):
             if "_crs" not in state:
                 state["_crs"] = None
             self.__dict__.update(state)
+
+    def to_wkb(self, hex: bool = False, **kwargs):
+        """Convert GeometryArray to a numpy object array of WKB objects."""
+        return shapely.to_wkb(self._data, hex=hex, **kwargs)
 
     # -------------------------------------------------------------------------
     # Geometry related methods
@@ -1528,7 +1533,7 @@ class GeometryArray(ExtensionArray):
         """
         # note ExtensionArray usage of value_counts only specifies dropna,
         # so sort, normalize and bins are not arguments
-        values = to_wkb(self)
+        values = self.to_wkb()
         from pandas import Index, Series
 
         result = Series(values).value_counts(dropna=dropna)
@@ -1693,7 +1698,7 @@ class GeometryArray(ExtensionArray):
             `na_sentinal` and not included in `uniques`. By default,
             ``np.nan`` is used.
         """
-        vals = to_wkb(self)
+        vals = self.to_wkb()
         return vals, None
 
     @classmethod
--- geopandas/geoseries.py
+++ geopandas/geoseries.py
@@ -26,7 +26,6 @@ from .array import (
     from_wkb,
     from_wkt,
     points_from_xy,
-    to_wkb,
     to_wkt,
 )
 from .base import is_geometry_type
@@ -1403,7 +1402,7 @@ e": "Feature", "properties": {}, "geometry": {"type": "Point", "coordinates": [3
         3                                                  NaN
         dtype: str
         """
-        return Series(to_wkb(self.array, hex=hex, **kwargs), index=self.index)
+        return Series(self.array.to_wkb(hex=hex, **kwargs), index=self.index)
 
     def to_wkt(self, **kwargs) -> Series:
         """Convert GeoSeries geometries to WKT.
