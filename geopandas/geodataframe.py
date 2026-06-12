22f119aaa9083c652570a58fff2a267ee7996d8c
barisoztas <baris@bariso.dev>
2026-06-12 17:44:35 +0200

    DOC: Add notes about WKB limitations to to_wkb methods (#3785)
    add note that linearrings are converted to linestrings and NaN points become empty points when using `to_wkb`.

--- geopandas/geodataframe.py
+++ geopandas/geodataframe.py
@@ -1286,6 +1286,15 @@ properties': {'col1': 'name1'}, 'geometry': {'type': 'Point', 'coordinates': (1.
         -------
         DataFrame
             geometry columns are encoded to WKB
+
+        Notes
+        -----
+        The Well-Known Binary (WKB) specification does not support all variations
+        of geometry types that GeoPandas does, and some geometries may not be
+        serialised without information loss. Notably:
+
+        - LinearRing will be converted to LineString
+        - Point with NaN coordinates will be converted to Empty Point
         """
         df = DataFrame(self.copy(deep=not PANDAS_GE_30))
 
