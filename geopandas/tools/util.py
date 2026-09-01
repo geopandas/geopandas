6bdbaf890d38e06e800fc883bd8ff050e1a05d15
Martin Fleischmann <martin@martinfleischmann.net>
2026-09-01 15:22:44 +0200

    Update util.py
    Co-authored-by: Joris Van den Bossche <jorisvandenbossche@gmail.com>

--- geopandas/tools/util.py
+++ geopandas/tools/util.py
@@ -26,7 +26,7 @@ def collect(x, multi=False):
     """
     warnings.warn(
         "geopandas.tools.collect is deprecated. "
-        "Use GeoDataFrame.dissolve(method='collec').geometry.item() instead.",
+        "Use GeoDataFrame.dissolve(method='collect').geometry.item() instead.",
         DeprecationWarning,
         stacklevel=2,
     )
