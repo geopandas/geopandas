4ef47cbf85a5c155fa36b3dc34f3bb6e5102dd32
Martin Fleischmann <martin@martinfleischmann.net>
2026-09-16 10:48:31 +0200

    better way

--- geopandas/_compat.py
+++ geopandas/_compat.py
@@ -12,10 +12,8 @@ import shapely
 PANDAS_GE_23 = Version(pd.__version__) >= Version("2.3.0")
 PANDAS_GE_30 = Version(pd.__version__) >= Version("3.0.0")
 PANDAS_GE_31 = Version(pd.__version__) >= Version("3.1.0.dev0")
-
-PANDAS_INFER_STR = (
-    (not PANDAS_GE_31) and PANDAS_GE_23 and pd.options.future.infer_string
-)
+PANDAS_GE_40 = Version(pd.__version__) >= Version("4.0.0.dev0")
+PANDAS_INFER_STR = PANDAS_GE_40 or (PANDAS_GE_23 and pd.options.future.infer_string)
 
 
 # -----------------------------------------------------------------------------
