18e957aacbf8924a93e0a73e1a938c5c9c8c71ae
Martin Fleischmann <martin@martinfleischmann.net>
2026-09-16 10:08:46 +0200

    TST:  avoid pd.options.future.infer_string on pandas dev

--- geopandas/_compat.py
+++ geopandas/_compat.py
@@ -12,7 +12,10 @@ import shapely
 PANDAS_GE_23 = Version(pd.__version__) >= Version("2.3.0")
 PANDAS_GE_30 = Version(pd.__version__) >= Version("3.0.0")
 PANDAS_GE_31 = Version(pd.__version__) >= Version("3.1.0.dev0")
-PANDAS_INFER_STR = PANDAS_GE_23 and pd.options.future.infer_string
+
+PANDAS_INFER_STR = (
+    (not PANDAS_GE_31) and PANDAS_GE_23 and pd.options.future.infer_string
+)
 
 
 # -----------------------------------------------------------------------------
